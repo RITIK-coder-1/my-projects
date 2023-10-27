0591b35de1aac9951836711f86b0379efa1583d7
RITIK <ritikprofessional111@gmail.com>
2023-10-27 12:12:33 +0530

    practised js

---
+++ PRACTICE/operators.js
@@ -0,0 +1,49 @@
+let num1 = 232432
+let num2 = 333
+let num3 = 34323
+
+let average = (num1 + num2 + num3)/3
+
+// console.log(average);
+
+let prinicipal = 10500
+let rate = 8
+let time = 2 
+
+let interest = (prinicipal*rate*time)/100
+let amount = interest + prinicipal
+
+// console.log(interest);
+// console.log(amount);
+
+// console.log(`Thus, your interest is ${`$${interest}`} and the amount that you have to pay: ${`$${amount}`}`);
+
+// console.log(typeof (22222**2222));
+
+// let a = 23
+// let b = a++
+// b = 25
+// let c = b++
+
+// let a = 44
+// let b = ++a
+// let c = ++b
+
+let a = 23
+let b = 33
+let c = a++ + ++b
+let d = ++a + ++b
+let e = a++ - b++
+
+// console.table([a, b, c, d, e])
+
+
+// let x = 23
+// let y = 22
+// y *= x
+
+// console.log(y);
+
+let number = +true
+let number2 = -"456"
+// console.log(number2);
