e70cbea3e43c8fa7e5120043a3777a7b63540da2
RITIK <ritikprofessional111@gmail.com>
2023-10-27 12:31:42 +0530

    practised js

---
+++ PRACTICE/type-coercion.js
@@ -0,0 +1,41 @@
+// --------------------------------------- STRING COERCION ------------------------------------- 
+
+// let a = `ritik`
+// let b = 23
+
+// let c = a + b
+
+// // console.log(c);
+// // console.log(typeof c);
+
+// let d = true
+// let f = "the statement is: " + d
+
+// console.log(f);
+// console.log(typeof f);
+
+// console.log("22" + 3 + 44);
+// console.log(2 + 44 + "23");
+// console.log("222" + 4*2);
+
+
+// -------------------------------- Number Coercion ----------------------------------------- 
+
+// console.log("ritik" * 2);
+// console.log(null - -(4));
+// console.log(false / 23);
+// console.log(true % false);
+
+// console.log("2" > 3);
+// console.log("2a" != 3);
+
+// console.log(2 > null);
+// console.log(2 < null);
+// console.log(2 == null);
+// console.log(2 <= null);
+
+// console.log(0 > null);
+// console.log(0 == null);
+// console.log(0 >= null);
+
+// loose equality doesn't change null into a number (0)
