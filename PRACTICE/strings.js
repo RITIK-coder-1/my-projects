bc4740eaa71c64a7c5a7aa297e1e7dfdb5991431
RITIK <ritikprofessional111@gmail.com>
2023-10-28 11:35:50 +0530

    practising strings

---
+++ PRACTICE/strings.js
@@ -0,0 +1,7 @@
+// let username = "ritik"
+// let age = 19
+
+// console.log(`hey, this is ${username} and my age is ${age}`); // string interpolation with a variable
+
+// let escapedString = `quotes: \n ritik`
+// console.log(escapedString);
