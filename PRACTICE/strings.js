a51f9a9dbd1deda798dc34437ff0197e1ecf4b4d
RITIK <ritikprofessional111@gmail.com>
2023-10-28 11:47:14 +0530

    practising strings

--- PRACTICE/strings.js
+++ PRACTICE/strings.js
@@ -1,7 +1,13 @@
-// let username = "ritik"
+// let MyName = "ritik"  // (Primitive String)
+// let myName = new String("ritik")  // (Object String)
 // let age = 19
 
-// console.log(`hey, this is ${username} and my age is ${age}`); // string interpolation with a variable
+// // console.log(`hey, this is ${myName} and my age is ${age}`); // string interpolation with a variable
 
-// let escapedString = `quotes: \n ritik`
-// console.log(escapedString);
+// // let escapedString = `quotes: \n ritik`
+// // console.log(escapedString);
+
+// console.log(MyName == myName); //true
+// console.log(myName === MyName); // false
+
+
