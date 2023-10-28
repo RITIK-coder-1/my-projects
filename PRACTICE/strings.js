f4af5851f01e0d4ed3c90ca87551f86cb700d7f8
RITIK <ritikprofessional111@gmail.com>
2023-10-28 12:52:52 +0530

    practised Strings

--- PRACTICE/strings.js
+++ PRACTICE/strings.js
@@ -13,12 +13,46 @@
 
 let statement = `Hey! This is Ritik!! Gonna be a top web developer!`
 
-console.log(statement[3]); // !
-console.log(statement[424]); // undefined
-console.log(statement[-2]); // undefined
-console.log(statement.charAt(0)); // H
-console.log(statement.charAt(424)); // blank space
-console.log(statement.charAt(-3)); // blank space
-console.log(statement.at(49)); // !
-console.log(statement.at(424)); // undefined
-console.log(statement.at(-50)); // H
+// console.log(statement[3]); // !
+// console.log(statement[424]); // undefined
+// console.log(statement[-2]); // undefined
+// console.log(statement["2"]); // y
+// console.log(statement["a"]); // undefined
+
+// console.log(statement.charAt(0)); // H
+// console.log(statement.charAt(424)); // blank space
+// console.log(statement.charAt(-3)); // blank space
+// console.log(statement.charAt("4")); // blank space
+// console.log(statement.charAt("b")); // NaN => 0 // H
+
+// console.log(statement.at(49)); // !
+// console.log(statement.at(424)); // undefined
+// console.log(statement.at(-50)); // H
+// console.log(statement.at("5")); // T
+// console.log(statement.at("cc")); // NaN => 0 // H
+
+
+
+// console.log(statement.codePointAt(0)); // 72
+// console.log("H".codePointAt(0)); // 72
+// console.log(statement.codePointAt(-1)); // undefined
+// console.log(statement.codePointAt("3")); // 33
+// console.log(statement.codePointAt("aa")); // NaN => 0 // 72
+
+// console.log(statement.charCodeAt(2)); // 121
+// console.log(statement.charCodeAt(-2)); // NaN
+// console.log(statement.charCodeAt("4")); // 32
+// console.log(statement.charCodeAt("ritik")); // NaN => 0 // 72
+
+
+
+// console.log(statement.concat(" A top One!"));
+
+
+
+// console.log(statement.endsWith("developer")); // false
+// console.log(statement.endsWith("developer!")); // true
+
+
+console.log(String.fromCharCode(72)); // H
+console.log(String.fromCodePoint(121)); // y
