57622b6bfd546df9f2b0d8a8a31a3c67aed27a0c
RITIK <ritikprofessional111@gmail.com>
2023-10-28 12:00:32 +0530

    practising strings

--- PRACTICE/strings.js
+++ PRACTICE/strings.js
@@ -11,3 +11,14 @@
 // console.log(myName === MyName); // false
 
 
+let statement = `Hey! This is Ritik!! Gonna be a top web developer!`
+
+console.log(statement[3]); // !
+console.log(statement[424]); // undefined
+console.log(statement[-2]); // undefined
+console.log(statement.charAt(0)); // H
+console.log(statement.charAt(424)); // blank space
+console.log(statement.charAt(-3)); // blank space
+console.log(statement.at(49)); // !
+console.log(statement.at(424)); // undefined
+console.log(statement.at(-50)); // H
