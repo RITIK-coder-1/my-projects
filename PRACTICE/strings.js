61e4bf8b809d9a3e48c9d6f49ad094e38193c3f6
RITIK <ritikprofessional111@gmail.com>
2023-10-30 15:26:49 +0530

    finished string

--- PRACTICE/strings.js
+++ PRACTICE/strings.js
@@ -1,10 +1,10 @@
-// let MyName = "ritik"  // (Primitive String)
-// let myName = new String("ritik")  // (Object String)
-// let age = 19
+let MyName = "ritik"  // (Primitive String)
+let myName = new String("ritik")  // (Object String)
+let age = 19
 
 // console.log(`hey, this is ${myName} and my age is ${age}`); // string interpolation with a variable
 
-// // let escapedString = `quotes: \n ritik`
+let escapedString = `quotes: \n ritik`
 // // console.log(escapedString);
 
 // console.log(MyName == myName); //true
