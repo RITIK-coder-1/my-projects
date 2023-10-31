26f36881f6b6079fd40c719dda4a0263cb217182
RITIK <ritikprofessional111@gmail.com>
2023-10-31 19:53:34 +0530

    isSafeInteger()

--- PRACTICE/number.js
+++ PRACTICE/number.js
@@ -42,3 +42,19 @@ let num6 = 1.21
 
 // console.log(Number.isNaN(num3)); // false
 // console.log(Number.isNaN(num5)); // true
+
+
+// Safe integers ==> -(2^53 - 1) to 2^53 - 1
+
+let num7 = 12233267813483
+let num8 = -num7
+let num9 = 1262835134023745601847568743560184371583892381638961065038301365036105615
+let num10 = 1871034873n
+
+// console.log(Number.isSafeInteger(num6)); // false // not an integer
+// console.log(Number.isSafeInteger(num7)); // true
+// console.log(Number.isSafeInteger(num8)); // true
+// console.log(Number.isSafeInteger(num9)); //  false
+// console.log(Number.isSafeInteger(num10)); // false
+// console.log(Number.isSafeInteger(Infinity)); // false
+// console.log(Number.isSafeInteger(NaN)); // false
