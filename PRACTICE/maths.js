5eff2c5cada286a799f7a6487b12a7e99e6398f9
RITIK <ritikprofessional111@gmail.com>
2023-10-31 23:37:50 +0530

    Practised and finished MATHS

---
+++ PRACTICE/maths.js
@@ -0,0 +1,40 @@
+// console.log(Math.PI);
+
+let a = -5
+
+// console.log(Math.abs(a)); // 5
+
+let b = 12.6
+let c = 13.4
+
+// console.log(Math.round(b)); // 13
+// console.log(Math.round(c)); // 13
+
+// console.log(Math.ceil(b)); // 13
+// console.log(Math.ceil(c)); // 14
+
+// console.log(Math.floor(b)); // 12
+// console.log(Math.floor(c)); // 13
+
+
+
+// console.log(Math.pow(2, 4)); // 16
+
+// console.log(Math.sqrt(4)); // 2
+
+
+// console.log(Math.min(1, 2, 3, 4)); // 1
+// console.log(Math.max(1, 2, 3, 4)); // 4
+
+
+// console.log(Math.random()); // values from 0 to 1
+// console.log(Math.floor((Math.random()*6) + 1)); // non-decimal values from 0 to 6 // helpful for dice-game
+
+
+let min = 25
+let max = 50
+
+// console.log(Math.random()*(max - min)); // values from 0 to 25
+// console.log(Math.floor(Math.random()*(max - min))); // non-decimal values from 0 to 25
+// console.log(Math.floor(Math.random()*((max - min) + 1))); // non-zero & non-decimal values from 0 to 25 
+// console.log(Math.floor(Math.random()*((max - min) + 1) + min)); // values from 25 to 50
