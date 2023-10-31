f0321e34a5041662e3f316a6f000ff2da239cb0e
RITIK <ritikprofessional111@gmail.com>
2023-10-31 19:10:06 +0530

    practised number

--- PRACTICE/number.js
+++ PRACTICE/number.js
@@ -2,8 +2,21 @@ let num1 = 23 // (Primitive Number)
 
 let num2 = new Number(23) // (Object Number)
 
-console.log(num1 === num2); // false
-console.log(num1); // 23
-console.log(typeof num1); // number
-console.log(num2); // [Number: 23]
-console.log(typeof num2); // object
+// console.log(num1 === num2); // false
+// console.log(num1); // 23
+// console.log(typeof num1); // number
+// console.log(num2); // [Number: 23]
+// console.log(typeof num2); // object
+
+
+// let a = 1.2
+// let b = 2.1
+// let c = b - a
+// let d = 0.9
+
+// console.log(c === d); // false
+// console.log(c); // 0.9000000000000001
+
+
+
+
