fddc58193e273ff0308a615a24b54b5db1888a3c
RITIK <ritikprofessional111@gmail.com>
2023-11-01 19:40:48 +0530

    finished DATE

--- PRACTICE/date.js
+++ PRACTICE/date.js
@@ -1,11 +1,16 @@
 let myDate = new Date()
 
-console.log(myDate); // 2023-11-01T06:26:22.466Z
-console.log(myDate.toString()); // "Wed Nov 01 2023 11:57:00 GMT+0530 (India Standard Time)"
+console.log(myDate); // 2023-11-01T13:38:50.082Z
+console.log(myDate.toString()); // "Wed Nov 01 2023 19:08:50 GMT+0530 (India Standard Time)"
 console.log(myDate.toDateString()); // "Wed Nov 01 2023"
-console.log(myDate.toISOString()); // "2023-11-01T08:02:02.887Z"
-console.log(myDate.toTimeString()); // 13:36:47 GMT+0530 (India Standard Time)
-console.log(myDate.toTimeString()); // 13:40:21 GMT+0530 (India Standard Time)
+console.log(myDate.toISOString()); // "2023-11-01T13:38:50.082Z"
+console.log(myDate.toTimeString()); // 19:08:50 GMT+0530 (India Standard Time)
+console.log(myDate.toJSON()); // "2023-11-01T13:38:50.082Z"
+console.log(myDate.toLocaleDateString()); // "11/1/2023"
+console.log(myDate.toLocaleDateString("en-IN")); // "1/11/2023"
+console.log(myDate.toLocaleString()); // 11/1/2023, 7:08:50 PM
+console.log(myDate.toLocaleTimeString()); // 7:26:42 PM
+console.log(myDate.valueOf()); // in ms
 
 
 console.log(Date.now()); // in ms
@@ -32,6 +37,19 @@ let myBirthday = new Date("August 20, 2004, 00:30:00")
 console.log(myBirthday); // 2004-08-19T19:00:00.000Z
 console.log(String(myBirthday)); // "Fri Aug 20 2004 00:30:00 GMT+0530 (India Standard Time)"
 
+console.log(myBirthday.getTime()); // in ms
+console.log(Math.floor((myBirthday.getTime())/(1000*60*60*24*365))); // in years // 34
+
+console.log(myBirthday.valueOf()); // in ms
+console.log(Math.floor(myBirthday.valueOf()/(1000*60*60*24*365))); // in years // 34
+
+// My Age ---------> 
+let TimeTillNow = Math.floor(Date.now()/(1000*60*60*24*365))
+let TimeTillMyBirthday = Math.floor((myBirthday.getTime())/(1000*60*60*24*365))
+let myAge = TimeTillNow - TimeTillMyBirthday // 53 - 34
+console.log(myAge); // 19
+
+
 console.log(myBirthday.getDate()); // 20
 console.log(myBirthday.getDay()); // 5 (Friday)
 console.log(myBirthday.getFullYear()); // 2004
@@ -50,8 +68,11 @@ console.log(myBirthday.setFullYear(2023));
 console.log(myBirthday.getFullYear()); // 2023
 console.log(myBirthday.setMinutes(35));
 console.log(myBirthday.getMinutes()); // 35
-console.log(myBirthday.toString()); // "Fri Aug 25 2023 00:35:00 GMT+0530 (India Standard Time)"
-// my birthday has been changed because it's an object (non-primitive)
+console.log(myBirthday.toString()); /* "Fri Aug 25 2023 00:35:00 GMT+0530 (India Standard Time)"
+my birthday has been changed because it's an object (non-primitive) */
+
+
+
 
 
 
