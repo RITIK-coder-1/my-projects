69c8cfe580bcdac0110daf51607824712f7242d2
RITIK <ritikprofessional111@gmail.com>
2023-11-03 19:12:37 +0530

    basics of functions

---
+++ PRACTICE/functions.js
@@ -0,0 +1,11 @@
+function Add(num1, num2) {
+    console.log(num1 + num2)
+}
+
+console.log(Add(1,2)); // 3
+
+function multiply(num1, num2){
+    console.log(num1*num2);
+}
+
+console.log(multiply(33,4)); // 132
