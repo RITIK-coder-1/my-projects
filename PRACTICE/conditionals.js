1733d6249264042c11249969860ea6130e9b4ad8
RITIK <ritikprofessional111@gmail.com>
2023-11-04 13:55:57 +0530

    condition to check the greater number out of two

---
+++ PRACTICE/conditionals.js
@@ -0,0 +1,42 @@
+// Condition to check the greater number out of two ----------->
+
+
+let num1 = 1
+let num2 = 2
+
+if (num1 > num2) {
+    console.log(`num1: ${num1} is greater than num2: ${num2}`);
+} else {
+    console.log(`num2: ${num2} is greater than num1: ${num1}`);
+}
+
+
+// if (typeof num1 === typeof num2 === Number) { 
+//     if (num1 > num2) {
+//         console.log("num1 is greater than num2");
+//     } if (num2 > num1) {
+//         console.log("num2 is greater than num1");
+//     } if (num1 === num2) {
+//         console.log("num1 equals to num2");
+//     }
+// } else {
+//     console.log("The input type is not a number!");
+// }
+
+// if (typeof num1 === typeof num2 === typeof 1) {
+//     if (num1 > num2) {
+//         console.log(`num1: ${num1} is greater than num2: ${num2}`);
+//     } else {
+//         console.log(`num2: ${num2} is greater than num1: ${num1}`);
+//     } if (num1 === num2) {
+//         console.log("Both the numbers are equal");
+//     }
+// } else {
+//     console.log("The input type is not a number!");
+// }
+
+
+
+
+
+
