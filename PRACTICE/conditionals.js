fad0dc4332729af7b3d5a4087b04f65194dad9ae
RITIK <ritikprofessional111@gmail.com>
2023-11-04 22:09:03 +0530

    practisingg

--- PRACTICE/conditionals.js
+++ PRACTICE/conditionals.js
@@ -1,10 +1,12 @@
 // Condition to check the greater number out of two ----------->
 
 
-// let num1 = 1
-// let num2 = 2
+// let num1 = 200
+// let num2 = 200
 
-// if (num1 > num2) {
+// if (num1 == num2) {
+//     console.log(`Both the numbers are equal!`);
+// } else if (num1 > num2) {
 //     console.log(`num1: ${num1} is greater than num2: ${num2}`);
 // } else {
 //     console.log(`num2: ${num2} is greater than num1: ${num1}`);
@@ -13,35 +15,54 @@
 
 // Condition to check the greater number out of three ---------->
 
-let num1 = 1600000000
-let num2 = 1700000000
-let num3 = 1700000000
+let num1 = 170000000000
+let num2 = 190000000000
+let num3 = 180000000000
 
-if (num1 == num2 && num2 == num3) {
-    console.log("All the numbers are equal");
+if (num1 === num2 && num2 === num3) {
+    console.log("All the numbers are equal!");
+} else if (num1 >= num2 && num2 > num3) {
+    console.log(`num1: ${num1} and num2: ${num2} are the greatest!`);
+} else if (num1 > num2 && num2 >= num3) {
+    console.log(`num1: ${num1} is the greatest!`);
+} else if (num2 >= num3 && num3 > num1) {
+    console.log(`num2: ${num2} and num3: ${num3} are the greatest!`);
+} else if (num2 > num3 && num3 >= num1) {
+    console.log(`num2: ${num2} is the greatest!`);
 } else {
-    if (num1 >= num2 && num1 >= num3) {
-        if (num2 >= num3 || num3 >= num2) {
-        console.log(`num1: ${num1} is the greatest`);
-    }
-    } else {
-        if (num2 >= num1 && num2 >= num3) {
-            if (num2 > num1 && num2 == num3) {
-                console.log(`num2: ${num2} & num3: ${num3} are the greatest`);
-            }
-        } else{
-                if (num1 >= num3 || num3 >= num1) {
-                console.log(`num2: ${num2} is the greatest`);
-                } else {
-                    if (num3 >= num2 && num3 >= num1) {
-                        if (num2 >= num1 || num1 >= num2) {
-                        console.log(`num3: ${num3} is the greatest`);
-                    }
-                }
-            }
-        }
-    }
+    console.log(`num3: ${num3} is the greatest!`);
 }
+
+
+
+// if (num1 == num2 && num2 == num3) {
+//     console.log("All the numbers are equal");
+// } else {
+//     if (num1 >= num2 && num1 >= num3) {
+//         if (num2 >= num3 || num3 >= num2) {
+//         console.log(`num1: ${num1} is the greatest`);
+//     }
+//     } else {
+//         if (num2 >= num1 && num2 >= num3) {
+//             if (num2 > num1 && num2 == num3) {
+//                 console.log(`num2: ${num2} & num3: ${num3} are the greatest`);
+//             }
+//         } else{
+//                 if (num1 >= num3 || num3 >= num1) {
+//                 console.log(`num2: ${num2} is the greatest`);
+//                 } else {
+//                     if (num3 >= num2 && num3 >= num1) {
+//                         if (num2 >= num1 || num1 >= num2) {
+//                         console.log(`num3: ${num3} is the greatest`);
+//                     }
+//                 }
+//             }
+//         }
+//     }
+// }
+
+
+
 
 
 
