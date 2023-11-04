b422c881238347bd3da1d2325826ede8a36937e5
RITIK <ritikprofessional111@gmail.com>
2023-11-04 15:12:46 +0530

    practsing conditionals

--- PRACTICE/conditionals.js
+++ PRACTICE/conditionals.js
@@ -1,14 +1,48 @@
 // Condition to check the greater number out of two ----------->
 
 
-let num1 = 1
-let num2 = 2
+// let num1 = 1
+// let num2 = 2
 
-if (num1 > num2) {
-    console.log(`num1: ${num1} is greater than num2: ${num2}`);
+// if (num1 > num2) {
+//     console.log(`num1: ${num1} is greater than num2: ${num2}`);
+// } else {
+//     console.log(`num2: ${num2} is greater than num1: ${num1}`);
+// }
+
+
+// Condition to check the greater number out of three ---------->
+
+let num1 = 1600000000
+let num2 = 1700000000
+let num3 = 1700000000
+
+if (num1 == num2 && num2 == num3) {
+    console.log("All the numbers are equal");
 } else {
-    console.log(`num2: ${num2} is greater than num1: ${num1}`);
+    if (num1 >= num2 && num1 >= num3) {
+        if (num2 >= num3 || num3 >= num2) {
+        console.log(`num1: ${num1} is the greatest`);
+    }
+    } else {
+        if (num2 >= num1 && num2 >= num3) {
+            if (num2 > num1 && num2 == num3) {
+                console.log(`num2: ${num2} & num3: ${num3} are the greatest`);
+            }
+        } else{
+                if (num1 >= num3 || num3 >= num1) {
+                console.log(`num2: ${num2} is the greatest`);
+                } else {
+                    if (num3 >= num2 && num3 >= num1) {
+                        if (num2 >= num1 || num1 >= num2) {
+                        console.log(`num3: ${num3} is the greatest`);
+                    }
+                }
+            }
+        }
+    }
 }
+
 
 
 // if (typeof num1 === typeof num2 === Number) { 
