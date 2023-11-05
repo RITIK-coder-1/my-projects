4ae8875759aa7a1d692a54a9bcc53dda50c83869
RITIK <ritikprofessional111@gmail.com>
2023-11-05 19:37:47 +0530

    practising loops

---
+++ PRACTICE/loops.js
@@ -0,0 +1,10 @@
+// Loop to prin the first 10 natural numbers -------->
+
+// for (let num = 1; num <= 10; num++) {
+//     console.log(num);    
+// }
+
+for (let num = 0; num <= 20 && num%2 == 0; num++) {
+    console.log(num);
+}
+
