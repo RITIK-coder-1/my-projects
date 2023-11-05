d39c620c352018a2621746809db25df1e1e5c711
RITIK <ritikprofessional111@gmail.com>
2023-11-05 21:26:55 +0530

    practised js

--- PRACTICE/conditionals.js
+++ PRACTICE/conditionals.js
@@ -42,29 +42,29 @@
 
 // Condition to check the greatest number and also check the type-------------->
 
-// let num1 = 11
-// let num2 = 11
-// let num3 = 11
+let num1 = 11
+let num2 = 11
+let num3 = 11
 
-// if (typeof num1 === 'number' && typeof num2 === 'number' && typeof num3 === 'number') {   
-//   if (num1 === num2 && num2 === num3) {
-//     console.log("All the numbers are equal!");
-// } else if (num1 === num2 && num1 > num3) {
-//     console.log(`num1: ${num1} and num2: ${num2} are the greatest!`);
-// } else if (num1 === num3 && num1 > num2){
-//     console.log(`num1: ${num1} and num3: ${num3} are the greatest!`);
-// } else if (num1 > num2 && num2 >= num3 && num3 >= num2) {
-//     console.log(`num1: ${num1} is the greatest!`);
-// } else if (num2 === num3 && num2 > num1){
-//     console.log(`num2: ${num2} and num3: ${num3} are the greatest!`);
-// } else if (num2 > num3 && num3 >= num1 && num3 <= num1){
-//     console.log(`num2: ${num2} is the greatest!`);
-// } else {
-//     console.log(`num3: ${num3} is the greatest!`);
-// }
-// } else {
-//     console.log("The input type is not a number!");
-// }
+if (typeof num1 === 'number' && typeof num2 === 'number' && typeof num3 === 'number') {   
+  if (num1 === num2 && num2 === num3) {
+    console.log("All the numbers are equal!");
+} else if (num1 === num2 && num1 > num3) {
+    console.log(`num1: ${num1} and num2: ${num2} are the greatest!`);
+} else if (num1 === num3 && num1 > num2){
+    console.log(`num1: ${num1} and num3: ${num3} are the greatest!`);
+} else if (num1 > num2 && num2 >= num3 && num3 >= num2) {
+    console.log(`num1: ${num1} is the greatest!`);
+} else if (num2 === num3 && num2 > num1){
+    console.log(`num2: ${num2} and num3: ${num3} are the greatest!`);
+} else if (num2 > num3 && num3 >= num1 && num3 <= num1){
+    console.log(`num2: ${num2} is the greatest!`);
+} else {
+    console.log(`num3: ${num3} is the greatest!`);
+}
+} else {
+    console.log("The input type is not a number!");
+}
 
 
 
