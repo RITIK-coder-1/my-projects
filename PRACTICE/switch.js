f7dc046cbeb8959b927df0a413bfa172ebcec421
RITIK <ritikprofessional111@gmail.com>
2023-11-05 14:43:54 +0530

    practised switch

---
+++ PRACTICE/switch.js
@@ -0,0 +1,10 @@
+// let color = "orange"
+
+// switch (color) {
+//     case "red":
+//     console.log("The color is red");
+//     break;
+//     default:
+//     console.log(('The color is not red'));
+//     break;
+// }
