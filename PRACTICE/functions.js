d2e03e52388b98cc0d1f5d02282af52cf1860675
RITIK <ritikprofessional111@gmail.com>
2023-11-06 14:54:12 +0530

    practised js

--- PRACTICE/functions.js
+++ PRACTICE/functions.js
@@ -1,11 +1,18 @@
-function Add(num1, num2) {
-    console.log(num1 + num2)
+// function Add(num1, num2) {
+//     console.log(num1 + num2)
+// }
+
+// console.log(Add(1,2)); // 3
+
+// function multiply(num1, num2){
+//     console.log(num1*num2);
+// }
+
+// console.log(multiply(33,4)); // 132
+
+
+function fun(name, surname){
+    console.log(`My name is: ${name} ${surname}.`);
 }
 
-console.log(Add(1,2)); // 3
-
-function multiply(num1, num2){
-    console.log(num1*num2);
-}
-
-console.log(multiply(33,4)); // 132
+fun("Ritik", "Mahapatra")
