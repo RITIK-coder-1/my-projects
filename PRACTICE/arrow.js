2508b85ff5995d9541c715b02696e58604ae2656
RITIK <ritikprofessional111@gmail.com>
2023-11-10 15:13:16 +0530

    practising functions

--- PRACTICE/arrow.js
+++ PRACTICE/arrow.js
@@ -6,3 +6,13 @@ const arrow2 = (anyString) => `${anyString} has ${anyString.length} number of ch
 
 // console.log(arrow2("ritik"));
 // console.log(arrow2("sonali"));
+
+const arrow3 = (anyArray) => {
+    for (let index = 0; index < anyArray.length; index++) {
+        if (typeof anyArray[index] === 'number') {
+            console.log(`The array [${anyArray}] has got number element(s) and the element(s): ${anyArray[index]}}`);
+        } 
+}
+}
+
+// console.log(arrow3(["ritik", 1, null]));
