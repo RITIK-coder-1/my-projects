c61e0ae754580bc2ba1b6d6b71713d9318674ef8
RITIK <ritikprofessional111@gmail.com>
2023-11-10 14:59:03 +0530

    practising arrow functions

--- PRACTICE/arrow.js
+++ PRACTICE/arrow.js
@@ -1,3 +1,8 @@
 const arrow1 = (num1, num2) =>  num1 + num2
 
-console.log(arrow1(1, 2));
+// console.log(arrow1(1, 2));
+
+const arrow2 = (anyString) => `${anyString} has ${anyString.length} number of characters`
+
+// console.log(arrow2("ritik"));
+// console.log(arrow2("sonali"));
