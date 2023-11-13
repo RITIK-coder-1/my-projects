de5d9311befacdbdca00e9fc94c5b16449ebc7d2
RITIK <ritikprofessional111@gmail.com>
2023-11-13 20:58:28 +0530

    practised scopes

--- PRACTICE/scope.js
+++ PRACTICE/scope.js
@@ -20,3 +20,27 @@ if (true) {
 
 // console.log(`outerName: ${name}`); // "RITIK"
 
+
+let num = 100
+
+let num1 = 1
+
+if (true) {
+    const num = 200
+    const num2 = 33
+    // console.log(num1); // 1
+    // console.log(num3); // ERROR
+    // console.log(num); // 200
+    if (true) {
+        const num = 300
+        // const num3 = 22 // ERROR
+        // console.log(num2); // 33
+        // console.log(num); // 300
+    }
+}
+
+// console.log(num); 100
+// console.log(num1); // 1
+// console.log(num2); // ERROR
+// console.log(num3); // ERROR
+
