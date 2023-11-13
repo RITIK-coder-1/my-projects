dbb492703d1ac9bc81695b19c5a1beb6e65ccf17
RITIK <ritikprofessional111@gmail.com>
2023-11-13 20:52:38 +0530

    created a new file, scope.js

---
+++ PRACTICE/scope.js
@@ -0,0 +1,22 @@
+if (true){
+    let a = "ritik"
+    const b = "Ritik"
+    var c = "RITIK"
+
+    // console.log(a, b, c);
+}
+
+// console.log(a); // ERROR
+// console.log(b); // ERROR
+// console.log(c); // RITIK
+
+
+let name = "RITIK"
+
+if (true) {
+    const name = "ritik"
+    // console.log(`innerName: ${name}`); // "ritik"
+}
+
+// console.log(`outerName: ${name}`); // "RITIK"
+
