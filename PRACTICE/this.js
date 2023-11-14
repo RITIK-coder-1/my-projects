4812993d073a1ab5eb6f5a94802e96a17206f4d2
RITIK <ritikprofessional111@gmail.com>
2023-11-14 11:10:31 +0530

    revising functions

--- PRACTICE/this.js
+++ PRACTICE/this.js
@@ -2,16 +2,20 @@
 
 const obj = {
     name: "ritik",
-    greet: function() {
+    greet1: function() {
     console.log(`Hello! ${this.name}`)
-},
-    currentContext: function(){
+    },
+    greet2: () => console.log(this.name),
+    currentContext1: function(){
         console.log(this);
-    }
+    },
+    currentContext2: () => console.log(this)
 }
 
-// obj.greet() // Hello! ritik
-// obj.currentContext()
+// obj.greet1() // Hello! ritik
+// obj.greet2() // undefined
+// obj.currentContext1()
+// obj.currentContext2() // {}
 
 function myName(name){
     console.log(this); // so many different values
