d1e82f86a0f8ce034730198a46374f380e52bfaf
RITIK <ritikprofessional111@gmail.com>
2023-11-15 19:31:56 +0530

    created a function for question one

--- PRACTICE/50_JS_QUESTIONS/one.js
+++ PRACTICE/50_JS_QUESTIONS/one.js
@@ -2,10 +2,35 @@
 
 let num = 0
 
-if (num === 0) {
-    console.log("It's a Zero!");
-} else if (num < 0) {
-    console.log(`The number ${num} is a negative integer!`);
+if (typeof num === 'number'){
+    if (num === 0) {
+        console.log("It's a Zero!");
+    } else if (num < 0) {
+        console.log(`The number ${num} is a negative integer!`);
+    } else {
+        console.log(`The number ${num} is a positive integer!`);
+    }
 } else {
-    console.log(`The number ${num} is a positive integer!`);
+    console.log(`Please! Input a number!`);
 }
+
+// FUNCTION ------------->
+
+function checkInteger(num) {
+    if (typeof num === 'number'){
+        if (num === 0) {
+            console.log("It's a Zero!");
+        } else if (num < 0) {
+            console.log(`The number ${num} is a negative integer!`);
+        } else {
+            console.log(`The number ${num} is a positive integer!`);
+        }
+    } else {
+        console.log(`Please! Input a number!`);
+    }
+}
+
+// checkInteger(10)
+// checkInteger(-1)
+// checkInteger(0)
+// checkInteger("null")
