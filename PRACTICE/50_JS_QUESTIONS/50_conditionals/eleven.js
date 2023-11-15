f4b7504eb5cd35099eb30e5ff98459e20c29181c
RITIK <ritikprofessional111@gmail.com>
2023-11-15 21:29:44 +0530

    finished 11th question

---
+++ PRACTICE/50_JS_QUESTIONS/50_conditionals/eleven.js
@@ -0,0 +1,8 @@
+// Implement a program that prompts the user for two numbers and swaps their values --->
+
+function swap(value1, value2) {
+    return `${value2},${value1}`
+}
+
+const result = swap(1, 3)
+console.log(result);
