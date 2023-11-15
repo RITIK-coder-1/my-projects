90d8b619430f964f8412b907e1e16b5954964c27
RITIK <ritikprofessional111@gmail.com>
2023-11-15 19:51:27 +0530

    modified my code and added its review

--- PRACTICE/50_JS_QUESTIONS/50_conditionals/two.js
+++ PRACTICE/50_JS_QUESTIONS/50_conditionals/two.js
@@ -3,14 +3,25 @@
 const largerNumber = (num1, num2) => {
     if (typeof num1 === 'number' && typeof num2 === 'number') {
         if (num1 > num2) {
-            console.log(`The number ${num1} is the largest!`);
+            let statement = `The number ${num1} is the largest!`
+            console.log(statement);
+            return statement
+        } else if (num2 > num1) {
+            let statement = `The number ${num2} is the largest!`
+            console.log(statement);
+            return statement
         } else {
-            console.log(`The number ${num2} is the largest!`);
+            let statement = "Both the numbers are equal!"
+            console.log(statement);
+            return statement
         }
     } else {
-        console.log("Both the input values should be numbers!");
+        let statement = "Both the input values should be numbers!"
+        console.log(statement);
+        return statement
     }
 }
 
 // largerNumber(123, 156)
 // largerNumber(1, "ritik")
+// largerNumber(12, 12)
