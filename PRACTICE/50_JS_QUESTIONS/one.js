28217e04651a617b8f927a1a5c4ef8680564f94c
RITIK <ritikprofessional111@gmail.com>
2023-11-15 19:18:32 +0530

    solved the first conditional question

---
+++ PRACTICE/50_JS_QUESTIONS/one.js
@@ -0,0 +1,11 @@
+// Write a JavaScript program to check if a number is positive, negative, or zero -------->
+
+let num = 0
+
+if (num === 0) {
+    console.log("It's a Zero!");
+} else if (num < 0) {
+    console.log(`The number ${num} is a negative integer!`);
+} else {
+    console.log(`The number ${num} is a positive integer!`);
+}
