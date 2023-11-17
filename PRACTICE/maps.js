8f72d0b1123c1faf515212cf1f21608bfbb9b427
RITIK <ritikprofessional111@gmail.com>
2023-11-17 12:21:49 +0530

    practising maps

---
+++ PRACTICE/maps.js
@@ -0,0 +1,10 @@
+const map = new Map()
+
+map.set("name", "ritik")
+map.set(1, "position")
+
+// console.log(map);
+
+// console.log(map.get("name")) // "ritik"
+// console.log(map.get(1)); // "position"
+// console.log(map.get("ritik")); // undefined
