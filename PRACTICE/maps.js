ca5b9231df75c8f4eaad0f8743a84015404252c4
RITIK <ritikprofessional111@gmail.com>
2023-11-17 12:36:12 +0530

    loops on a map

--- PRACTICE/maps.js
+++ PRACTICE/maps.js
@@ -4,17 +4,25 @@ map.set("name", "ritik")
 map.set(1, "position")
 map.set(1, "position")
 
-console.log(map);
+// console.log(map);
 // console.log(typeof map); // object
 
 // console.log(map.get("name")) // "ritik"
 // console.log(map.get(1)); // "position"
 // console.log(map.get("ritik")); // undefined
 
-console.log(map.size); // 2
+// console.log(map.size); // 2
 
 map.set(null, "trail")
 // console.log(map);
 
 map.delete(null)
 // console.log(map);
+
+for (const element of map) {
+    // console.log(element);
+}
+
+for (const [key, value] of map){
+    // console.log(`The key: ${key} & The value: ${value}`);
+}
