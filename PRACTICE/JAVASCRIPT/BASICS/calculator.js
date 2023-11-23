c6877090899f1a9831a2c743f31f3fbdf88e37df
RITIK <ritikprofessional111@gmail.com>
2023-11-23 23:55:31 +0530

    practised

---
+++ PRACTICE/JAVASCRIPT/BASICS/calculator.js
@@ -0,0 +1,19 @@
+const add = (num1, num2) => {
+    console.log(num1 + num2);
+    return num1 + num2
+}
+
+const sub = (num1, num2) => {
+    console.log(num1 - num2);
+    return num1 - num2
+}
+
+const multiply = (num1, num2) => {
+    console.log(num1 * num2);
+    return num1 * num2
+}
+
+const division = (num1, num2) => {
+    console.log(num1 / num2);
+    return num1 / num2
+}
