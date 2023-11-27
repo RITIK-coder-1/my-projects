17b014f80b0db46db05f57da564ccfc179758b19
RITIK <ritikprofessional111@gmail.com>
2023-11-28 00:56:07 +0530

    practising DOM

--- PRACTICE/JAVASCRIPT/DOM-MANIPULATION/Selectors/style.js
+++ PRACTICE/JAVASCRIPT/DOM-MANIPULATION/Selectors/style.js
@@ -5,3 +5,17 @@ firstHeading.style.color = "green"
 firstHeading.style.backgroundColor = "blue"
 
 firstHeading.style.padding = "20px"
+
+const li = document.querySelectorAll("li")
+
+const arrayLi = Array.from(li)
+
+arrayLi.map((ele) => { 
+    ele.style.color = "blue"
+    ele.style.backgroundColor = "green"
+})
+
+const para = document.querySelectorAll("p")
+
+para[1].style.fontSize = "50px"
+
