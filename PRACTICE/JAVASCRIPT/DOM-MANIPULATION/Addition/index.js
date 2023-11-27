bba2bf43d953dcd32c16c86d93a26a21de38a7b0
RITIK <ritikprofessional111@gmail.com>
2023-11-28 01:11:38 +0530

    practising DOM

--- PRACTICE/JAVASCRIPT/DOM-MANIPULATION/Addition/index.js
+++ PRACTICE/JAVASCRIPT/DOM-MANIPULATION/Addition/index.js
@@ -28,3 +28,22 @@ body.append(para)
 
 // adding a styled div ------->
 
+const div = document.createElement("div")
+
+div.style.border = "2px solid red"
+div.style.height = "250px"
+div.style.width = "250px"
+div.style.backgroundColor = "blue"
+div.style.display = "flex"
+div.style.justifyContent = "center"
+div.style.alignItems = "center"
+div.style.fontFamily = "Sans-serif"
+div.style.fontWeight = "900"
+
+
+const divText = "this is a JS generated text"
+
+
+div.append(divText)
+
+body.append(div)
