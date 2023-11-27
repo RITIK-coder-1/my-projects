867cacfddc1313b14c37c73756078795a5664d57
RITIK <ritikprofessional111@gmail.com>
2023-11-28 00:51:06 +0530

    practising DOM

---
+++ PRACTICE/JAVASCRIPT/DOM-MANIPULATION/Selectors/style.js
@@ -0,0 +1,7 @@
+const firstHeading = document.getElementById("first heading")
+
+firstHeading.style.color = "green"
+
+firstHeading.style.backgroundColor = "blue"
+
+firstHeading.style.padding = "20px"
