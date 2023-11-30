15fd0a02d977e2700a48c7e7700995c600321f9c
RITIK <ritikprofessional111@gmail.com>
2023-12-01 00:32:36 +0530

    making my code better of color changer

--- 03_Basic_JavaScript_Projects/BackgroundColor_Changer/index.js
+++ 03_Basic_JavaScript_Projects/BackgroundColor_Changer/index.js
@@ -1,5 +1,9 @@
 const body = document.body
+body.style.backgroundColor = "black"
+let isDark = true
+
 const statement = document.querySelector("#set-color")
+statement.textContent = "BLACK color is set by default"
 
 const firstColor = document.querySelector(".one")
 const secondColor = document.querySelector(".two")
@@ -12,41 +16,125 @@ const eighthColor = document.querySelector(".eight")
 
 
 firstColor.addEventListener("click", () => {
-    body.style.backgroundColor = "#8D3DAF"
-    statement.textContent = "The Color has been set to PURPLE"
+
+    isDark = !isDark
+
+    if(isDark){
+        body.style.backgroundColor = "black"
+        statement.textContent = "BLACK color is set by default"
+    } else {
+        body.style.backgroundColor = "#8D3DAF"
+        statement.textContent = "The Color has been set to PURPLE"
+    }
+
 }, false)
 
 secondColor.addEventListener("click", () => {
-    body.style.backgroundColor = "#E07C24"
-    statement.textContent = "The Color has been set to TANGERINE"
+
+    isDark = !isDark
+
+    if(isDark){
+        body.style.backgroundColor = "black"
+        statement.textContent = "BLACK color is set by default"
+    } else {
+        body.style.backgroundColor = "#E07C24"
+        statement.textContent = "The Color has been set to TANGERINE"
+    }
+
 }, false)
 
 thirdColor.addEventListener("click", () => {
+
+    isDark = !isDark
+
+    if(isDark){
+        body.style.backgroundColor = "black"
+        statement.textContent = "BLACK color is set by default"
+    } else {
+        body.style.backgroundColor = "#8D3DAF"
+        statement.textContent = "The Color has been set to PURPLE"
+    }
+
     body.style.backgroundColor = "#758283"
     statement.textContent = "The Color has been set to SLATE BLUE"
 }, false)
 
 fourthColor.addEventListener("click", () => {
+
+    isDark = !isDark
+
+    if(isDark){
+        body.style.backgroundColor = "black"
+        statement.textContent = "BLACK color is set by default"
+    } else {
+        body.style.backgroundColor = "#8D3DAF"
+        statement.textContent = "The Color has been set to PURPLE"
+    }
+
     body.style.backgroundColor = "#E0388B"
     statement.textContent = "The Color has been set to DEEP PINK"
 }, false)
 
 fifthColor.addEventListener("click", () => {
+
+    isDark = !isDark
+
+    if(isDark){
+        body.style.backgroundColor = "black"
+        statement.textContent = "BLACK color is set by default"
+    } else {
+        body.style.backgroundColor = "#8D3DAF"
+        statement.textContent = "The Color has been set to PURPLE"
+    }
+
     body.style.backgroundColor = "#FF6666"
     statement.textContent = "The Color has been set to REDDISH PINK"
 }, false)
 
 sixthcolor.addEventListener("click", () => {
+
+    isDark = !isDark
+
+    if(isDark){
+        body.style.backgroundColor = "black"
+        statement.textContent = "BLACK color is set by default"
+    } else {
+        body.style.backgroundColor = "#8D3DAF"
+        statement.textContent = "The Color has been set to PURPLE"
+    }
+
     body.style.backgroundColor = "#03C6C7"
     statement.textContent = "The Color has been set to AQUA"
 }, false)
 
 seventhColor.addEventListener("click", () => {
+
+    isDark = !isDark
+
+    if(isDark){
+        body.style.backgroundColor = "black"
+        statement.textContent = "BLACK color is set by default"
+    } else {
+        body.style.backgroundColor = "#8D3DAF"
+        statement.textContent = "The Color has been set to PURPLE"
+    }
+
     body.style.backgroundColor = "#CAD5E2"
     statement.textContent = "The Color has been set to LIGHT BLUE"
 }, false)
 
 eighthColor.addEventListener("click", () => {
+
+    isDark = !isDark
+
+    if(isDark){
+        body.style.backgroundColor = "black"
+        statement.textContent = "BLACK color is set by default"
+    } else {
+        body.style.backgroundColor = "#8D3DAF"
+        statement.textContent = "The Color has been set to PURPLE"
+    }
+
     body.style.backgroundColor = "#35BDD0"
     statement.textContent = "The Color has been set to SKY BLUE"
 }, false)
