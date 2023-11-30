d200c15f6abcffcead7b7b9516d6837930def7fc
RITIK <ritikprofessional111@gmail.com>
2023-11-30 12:45:17 +0530

    made my code better & added  styles to counter

--- 03_Basic_JavaScript_Projects/Counter/index.js
+++ 03_Basic_JavaScript_Projects/Counter/index.js
@@ -3,11 +3,27 @@ const num = document.querySelector("#number")
 const decrease = document.querySelector("#decrement")
 
 increase.addEventListener("click", () => {
+
    num.textContent = Number(num.textContent) + 1
+
+   const button = document.querySelector("#increment")
+   button.style.animation = "none"
+   void button.offsetWidth
+   button.style.animation = "button 0.2s linear 0s 1 normal"
+
 }, false)
+
 decrease.addEventListener("click", () => {
+
    num.textContent = Number(num.textContent) - 1
+
     if (num.textContent < 0){
         alert("Check out! The Counter is going beyond 0.")
     }
+
+   const button = document.querySelector("#decrement")
+   button.style.animation = "none"
+   void button.offsetWidth
+   button.style.animation = "button 0.2s linear 0s 1 normal"
+
 }, false)
