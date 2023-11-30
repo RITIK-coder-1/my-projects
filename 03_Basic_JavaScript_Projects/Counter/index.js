95b78596c129cfa33beab75ace812673d3a79e50
RITIK <ritikprofessional111@gmail.com>
2023-11-30 12:37:44 +0530

    made my code better

--- 03_Basic_JavaScript_Projects/Counter/index.js
+++ 03_Basic_JavaScript_Projects/Counter/index.js
@@ -3,10 +3,10 @@ const num = document.querySelector("#number")
 const decrease = document.querySelector("#decrement")
 
 increase.addEventListener("click", () => {
-   num.textContent++
+   num.textContent = Number(num.textContent) + 1
 }, false)
 decrease.addEventListener("click", () => {
-    num.textContent--
+   num.textContent = Number(num.textContent) - 1
     if (num.textContent < 0){
         alert("Check out! The Counter is going beyond 0.")
     }
