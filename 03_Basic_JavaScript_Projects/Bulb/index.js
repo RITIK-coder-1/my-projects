7eca0452015ba7d6e6ed4a70e6c492bd589a39ac
RITIK <ritikprofessional111@gmail.com>
2023-12-01 14:48:56 +0530

    modified

--- 03_Basic_JavaScript_Projects/Bulb/index.js
+++ 03_Basic_JavaScript_Projects/Bulb/index.js
@@ -1,18 +1,25 @@
 const body = document.body
+body.style.backgroundColor = "black"
+
 const button = document.getElementById("switch")
 button.textContent = "TURN ON"
+
 const offBulb = document.getElementById("off")
 offBulb.style.visibility = "visible"
+
 const onBulb = document.getElementById("on")
 onBulb.style.visibility = "hidden" 
-body.style.backgroundColor = "black"
+
 let isDark = true
+
 const animatedButton = (button) => {
+
     button.style.animation = "none"
     void button.offsetWidth
     button.style.animation = "switch 0.1s ease-in-out 0s 1 normal"
 
 }
+
 button.addEventListener("click", () => {
     
     if (isDark){
