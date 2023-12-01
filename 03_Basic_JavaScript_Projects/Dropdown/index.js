8b7c0f91f7cba4789b339b3cded1ff49d0334d82
RITIK <ritikprofessional111@gmail.com>
2023-12-01 20:35:42 +0530

    finished my dropdown project

---
+++ 03_Basic_JavaScript_Projects/Dropdown/index.js
@@ -0,0 +1,11 @@
+const dropdown = ["JavaScript", "Java", "Python", "C", "C++"]
+
+const selectElements = document.getElementById("dropdown")
+
+dropdown.forEach((language) => {
+    const Option = document.createElement("option")
+    Option.value = language
+    Option.textContent = language
+    selectElements.append(Option)
+    console.log(language);
+})
