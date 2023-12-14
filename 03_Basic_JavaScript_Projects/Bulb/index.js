738cc62bd6ad0c5b7fcf85ef923eb5c28c789922
RITIK <ritikprofessional111@gmail.com>
2023-12-14 23:53:47 +0530

    holding the feature for now

--- 03_Basic_JavaScript_Projects/Bulb/index.js
+++ 03_Basic_JavaScript_Projects/Bulb/index.js
@@ -11,13 +11,13 @@ const yellowBulb = document.getElementById("on-yellow")
 yellowBulb.style.visibility = "hidden" 
 
 let isDark = true
-let Blue = false
+// let Blue = false
 
-const blue = document.querySelector("option[value='blue']")
+// const blue = document.querySelector("option[value='blue']")
 
-blue.addEventListener("click", () => {
-    isBlue = true
-})
+// blue.addEventListener("click", () => {
+//     isBlue = true
+// })
 
 const animatedButton = (button) => {
 
@@ -34,11 +34,11 @@ button.addEventListener("click", () => {
         yellowBulb.style.visibility = "visible"
         offBulb.style.visibility = "hidden"
         button.textContent = "TURN OFF"
-    } else if (isDark && Blue){
-        body.style.backgroundColor = "#6060a7"
-        yellowBulb.style.visibility = "visible"
-        offBulb.style.visibility = "hidden"
-        button.textContent = "TURN OFF"
+    // } else if (isDark && Blue){
+    //     body.style.backgroundColor = "#6060a7"
+    //     yellowBulb.style.visibility = "visible"
+    //     offBulb.style.visibility = "hidden"
+    //     button.textContent = "TURN OFF"
     } else {
         body.style.backgroundColor = "black"
         yellowBulb.style.visibility = "hidden"
