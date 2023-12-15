0cf8fa1fef1e430b2b616b849de1be336fea6e8a
RITIK <ritikprofessional111@gmail.com>
2023-12-15 13:26:10 +0530

    added a dropdown to my bulb

--- 03_Basic_JavaScript_Projects/Bulb/index.js
+++ 03_Basic_JavaScript_Projects/Bulb/index.js
@@ -27,6 +27,28 @@ const animatedButton = (button) => {
 
 }
 
+const dropdown = ["JavaScript", "Java", "Python", "C", "C++"]
+
+const selectElements = document.getElementById("dropdown")
+
+dropdown.forEach((language) => {
+
+    const option = document.createElement("option")
+    option.value = language
+    option.textContent = language
+
+    selectElements.append(option)
+})
+
+selectElements.addEventListener("change", function() {
+
+    const selectedOption = this.options[this.selectedIndex];
+
+    const p = document.querySelector("p")
+    p.innerText = `Your Favourite Programming Language has been set to: ${selectedOption.value}`
+
+});
+
 button.addEventListener("click", () => {
     
     if (isDark){
