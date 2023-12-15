c31ccc0d3b7363e08b8bbacad1e2a08b58309a0a
RITIK <ritikprofessional111@gmail.com>
2023-12-15 14:09:02 +0530

    added different customisable colors to my bulb

--- 03_Basic_JavaScript_Projects/Bulb/index.js
+++ 03_Basic_JavaScript_Projects/Bulb/index.js
@@ -7,10 +7,16 @@ button.textContent = "TURN ON"
 const offBulb = document.getElementById("off")
 offBulb.style.visibility = "visible"
 
-const yellowBulb = document.getElementById("on-yellow")
+const yellowBulb = document.getElementById("yellow-bulb")
 yellowBulb.style.visibility = "hidden" 
 
-let isDark = true
+const blueBulb = document.getElementById("blue-bulb")
+blueBulb.style.visibility = "hidden"
+
+const greenBulb = document.getElementById("green-bulb")
+greenBulb.style.visibility = "hidden"
+
+let isOff = true
 
 const animatedButton = (button) => {
 
@@ -36,23 +42,37 @@ dropdown.forEach((language) => {
 selectElements.addEventListener("change", function() {
 
     const selectedOption = this.options[this.selectedIndex];
-    
+
 }, false);
 
 button.addEventListener("click", () => {
     
-    if (isDark){
+    if (isOff && selectElements.value === "Yellow"){
         body.style.backgroundColor = "#f3f391"
         yellowBulb.style.visibility = "visible"
         offBulb.style.visibility = "hidden"
         button.textContent = "TURN OFF"
-    } else {
+    } else if (isOff && selectElements.value === "Blue"){
+        body.style.backgroundColor = "#9b9bc0"
+        blueBulb.style.visibility = "visible"
+        offBulb.style.visibility = "hidden"
+        button.textContent = "TURN OFF"
+    } 
+    else if (isOff && selectElements.value === "Green"){
+        body.style.backgroundColor = "#5eb35e"
+        greenBulb.style.visibility = "visible"
+        offBulb.style.visibility = "hidden"
+        button.textContent = "TURN OFF"
+    } 
+    else {
         body.style.backgroundColor = "black"
         yellowBulb.style.visibility = "hidden"
+        blueBulb.style.visibility = "hidden"
+        greenBulb.style.visibility = "hidden"
         offBulb.style.visibility = "visible"
         button.textContent = "TURN ON"
     }
-    isDark = !isDark
+    isOff = !isOff
 
     animatedButton(button)
 }, false)
