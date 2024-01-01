9d883f7a72fd6d75977f7b3fc1d2880c4be2dd68
RITIK <ritikprofessional111@gmail.com>
2024-01-01 15:44:44 +0530

    working on my project

--- 03_Basic_JavaScript_Projects/RandomColor_Generator/index.js
+++ 03_Basic_JavaScript_Projects/RandomColor_Generator/index.js
@@ -8,17 +8,24 @@ const randomColor = ["0", "1", "2", "3", "4", "5", "6", "A", "B", "C", "D", "E",
 
 const generateRandomColor = () => {
     let Color = "#"
-    for (let i = 0; i < randomNumber(); i++) {
-        const Element = randomColor[i]
-        Color += Element
-        console.log(Element);
-    }
-    // console.log(Element);
-    console.log(randomNumber())
+    // for (let i = 0; i < randomNumber(); i++) {
+    //     const Element = randomColor[i]
+    //     Color += Element
+    //     console.log(Element);
+    // }
+    // // console.log(Element);
+    randomNumber()
 }
 
 const randomNumber = () => {
 let min = 6
 let max = 13
-    return Math.floor(Math.random() * ((max - min) + 1) + min)
+const random = Math.floor(Math.random() * ((max - min) + 1) + min)
+
+for (let i = 0; i < random; i++) {
+    const Element = randomColor[i]
+    console.log(Element);
+}
+
+console.log(random);
 }
