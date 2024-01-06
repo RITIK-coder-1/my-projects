996bb96c24270a763ec26fa306a489c6b0ad5926
RITIK <ritikprofessional111@gmail.com>
2024-01-06 20:17:57 +0530

    modified

--- 03_Basic_JavaScript_Projects/RandomColor_Generator/index.js
+++ 03_Basic_JavaScript_Projects/RandomColor_Generator/index.js
@@ -3,10 +3,12 @@ const body = document.body
 
 button.addEventListener('click', () => {
     let bodyStyle = generateRandomColor()
-    let buttonStyle = `#${reverseString(bodyStyle).slice(0, 6)}`
+    let buttonStyle = `#${reverseString(bodyStyle).slice(0, 6)}` // opposite the color of the background // I sliced it to not include the "#" at the end
     body.style.backgroundColor = `${bodyStyle}`
     button.style.backgroundColor = `${buttonStyle}`
     animatedButton(button)
+    console.log(`1: Background Color: ${bodyStyle}`);
+    console.log(`2: Button Color: ${buttonStyle}`);
 })
 
 const randomColor = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"]
