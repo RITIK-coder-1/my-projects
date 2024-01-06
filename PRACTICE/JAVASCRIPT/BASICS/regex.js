3ed8f0ceea17f1204def1aa8df6d1c1e13aa5cd5
RITIK <ritikprofessional111@gmail.com>
2024-01-06 22:16:46 +0530

    practised regex

---
+++ PRACTICE/JAVASCRIPT/BASICS/regex.js
@@ -0,0 +1,11 @@
+let regEx1 = new RegExp("hello")
+let regEx2 = /Hello/
+
+// console.log(typeof regEx); // object
+
+let statement1 = "hello Ritik!"
+
+// console.log(statement1.match(regEx1));
+// console.log(regEx1.test(statement1)); // true
+// console.log(regEx2.test(statement1)); // false
+
