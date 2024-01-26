b181df0a988df2c9110f7cb283255d7a066e1fdc
RITIK <ritikprofessional111@gmail.com>
2024-01-26 22:55:45 +0530

    practice

--- PRACTICE/JAVASCRIPT/Async/promise.js
+++ PRACTICE/JAVASCRIPT/Async/promise.js
@@ -4,7 +4,6 @@ const myPromise = new Promise((resolve, reject) => {
     setTimeout(() => {
         console.log("executed");
         resolve() // it resolves the promise after timeout
-        reject()
     }, 2000)
     // resolve() // it resolves the promise before timeout
 })
