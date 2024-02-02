18c5a66032dbcf66e7ea1bd2426781b5d87f7358
RITIK <ritikprofessional111@gmail.com>
2024-02-02 12:46:59 +0530

    started a new quote generator project

---
+++ 03_Basic_JavaScript_Projects/Random_Quote_Generator/index.js
@@ -0,0 +1,31 @@
+let category = "happiness"
+let myAPI = fetch("https://type.fit/api/quotes")
+
+myAPI
+.then(response => {
+    return response.json()
+})
+.then(data => {
+    const button = document.getElementById("button")
+    const quote = document.querySelector("blockquote")
+    const author = document.querySelector("span")
+
+    button.addEventListener("click", () => {
+        quote.textContent = `"${data[generateRandomNumber()].text}"`
+
+        const authorString = data[generateRandomNumber()].author
+        if (authorString.includes("type.fit")) {
+            author.textContent = `-- ${authorString.substring(0, authorString.length - 10)}`
+        } // the default value is "author name, type.fit", thus to discard "type.fit" I wrote this!
+
+        if (authorString.includes("type.fit") && authorString.length === 9) {
+            author.textContent = "Unknown"
+        }
+    })
+
+    console.log(data)
+})
+
+function generateRandomNumber() {
+    return Math.floor(Math.random()*16)
+}
