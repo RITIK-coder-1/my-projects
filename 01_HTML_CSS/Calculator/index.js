7da5857512680c697e1954fdfd3b5d3a226c4ad7
RITIK <ritikprofessional111@gmail.com>
2024-02-06 20:45:01 +0530

    better my code of calculator

---
+++ 01_HTML_CSS/Calculator/index.js
@@ -0,0 +1,16 @@
+let num1 = 0, num2 = 0
+
+const one = document.getElementById("one")
+const two = document.getElementById("two")
+const three = document.getElementById("three")
+
+const add = document.getElementById("add")
+const equals = document.getElementById("equals")
+
+one.addEventListener("click", () => {
+    
+})
+two.addEventListener("click", () => {
+    
+})
+
