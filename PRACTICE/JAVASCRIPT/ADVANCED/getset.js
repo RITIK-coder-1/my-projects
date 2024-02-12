09dd952650eac0152d96e697915990d2c4c1cd57
RITIK <ritikprofessional111@gmail.com>
2024-02-12 12:08:38 +0530

    I learned getters and setters

--- PRACTICE/JAVASCRIPT/ADVANCED/getset.js
+++ PRACTICE/JAVASCRIPT/ADVANCED/getset.js
@@ -1,17 +1,65 @@
-const obj = {
-    name: "ritik",
-    email: "rtk@gmail.com",
+// Objects -->
 
-    get getEmail(){
-        return this.email + "abc"
+const obj = {
+    _name: "ritik", // conventionally privatised
+    _age: 19,
+
+    get name(){
+        return `abc${this._name}cba`
     },
 
-    set setEmail(value){
-        this.email = value + "abc" 
+    set age(value){
+        if (value > 0 && value < 100){
+            this._age = value
+        } else {
+            console.log("Invalid age");
+        }
     }
 }
 
-obj.email = "Ritik@ai.com"
+console.log(obj.name); // abcritikcba
 
-console.log(obj.getEmail);
-console.log(obj.email);
+obj.age = 30
+
+console.log(obj._age); // 30
+
+obj.age = 120
+
+console.log(obj._age); // Invalid age // 30
+
+// Class --->
+
+class newClass {
+    #name // privatised
+    #age
+
+    constructor(name, age){
+        this.#name = name
+        this.#age = name
+    }
+
+    get name(){
+        return `abc${this.#name}cba`
+    }
+
+    get age(){
+        return this.#age
+    }
+
+    set age(value){
+        if (value > 0 && value < 100) this.#age = value
+        else console.log("Invalid");
+    }
+}
+
+const user = new newClass("ritik", 19)
+
+console.log(user.name); // abcritikcba
+
+user.age = 30
+
+console.log(user.age); // 30
+
+user.age = 200
+
+console.log(user.age); // Invalid // 30
