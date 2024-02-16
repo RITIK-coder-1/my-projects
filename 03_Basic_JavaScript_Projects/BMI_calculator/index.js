e3a26b8d748c499338f7566a48404186eedb5be3
RITIK <ritikprofessional111@gmail.com>
2024-02-16 17:45:14 +0530

    I completed my BMI calculator with decent features

--- 03_Basic_JavaScript_Projects/BMI_calculator/index.js
+++ 03_Basic_JavaScript_Projects/BMI_calculator/index.js
@@ -1,4 +1,5 @@
-// type of calculation -->
+// Type of calculation -->
+
 let generalCalculation = true
 let metricCalculation = false
 
@@ -18,102 +19,152 @@ const metric = document.querySelector("#metric-button")
 const weightUnit = document.getElementById("weightUnit")
 const heightUnit = document.getElementById("heightUnit")
 
-// Calculation -->
+// Calculation --->
 
+// General Calculator --->
 
-    calculate1.addEventListener("click", () => {
-        calculate(weight1, height1, 0)
-    })
+calculate1.addEventListener("click", () => {
+    calculate(weight1.value, height1.value, 0)
+})
 
-    calculate2.addEventListener("click", () => {
-          let weight = 0
-          let height = 0 
-          calculate(weight2, height2, 1)
-    })
+// Metric Calculator --->
 
-// function to calculate --->
+// Variables for metric calculator --->
+
+let weight = Number(weight2.value)
+let height = Number(height2.value)
+
+// Event listeners for unit changes in metric calculator -->
+
+weightUnit.addEventListener("change", function() {
+    const selectedOption = this.options[this.selectedIndex].value
+    
+    switch (selectedOption) {
+        case "KiloGrams":
+            weight = Number(weight2.value)
+            break
+        case "Grams":
+            weight = Number(weight2.value) / 1000
+            break
+        case "Pounds":
+            weight = Number(weight2.value) * 0.453592
+            break
+    }
+})
+
+heightUnit.addEventListener("change", function() {
+    const selectedOption = this.options[this.selectedIndex].value
+    switch (selectedOption) {
+        case "Meters":
+            height = (height2.value)
+            break
+        case "Centimeters":
+            height = (height2.value) / 100
+            break
+        case "Inches":
+            height = (height2.value) * 0.0254
+            break
+    }
+})
+
+// Calculation for metric calculator --->
+
+calculate2.addEventListener("click", () => {
+    // Updating the weight and height based on the selected units --> 
+
+    weightUnit.dispatchEvent(new Event("change"))
+    heightUnit.dispatchEvent(new Event("change"))
+    
+    calculate(weight, height, 1)
+})
+
+// Function to calculate BMI --->
 
 function calculate(weight, height, indexNumber) { 
-    let BMI = (weight.value / (height.value * height.value)).toFixed(2)
-                for (let i = 0; i < result.length; i++) {
-                    const element = result[indexNumber]; // specific index number for specific calculator
-                    if (!(weight.value === "" || height.value === "")) {
-                        if (!(weight.value <= 0 || height.value <= 0)) {
-                            element.innerHTML = `BMI: ${BMI} kg/m<sup>2</sup>`
-                        } else {
-                            element.innerHTML = "Please enter a valid number!" // if a negative integer or 0 has been input
-                        }
-                    } else {
-                        element.innerHTML = "Please fill all the required fields!" // if either of input field is empty
-                    }
-                }
-            
-            
-            // Conclusion to display after the result -->
-            
-            let summary1 = ""
-            let summary2 = ""
-            if (!(weight.value === "" || height.value === "") && !(weight.value <=0 || height.value <=0)) {
-                switch (true) {
-                    case BMI > 0 && BMI < 16:
-                        summary1 = "You are Underweight!"
-                        summary2 = "(You are Severly Thin!!)"
-                        break;      
-                    case BMI > 16 && BMI < 17:
-                        summary1 = "You are Underweight!"
-                        summary2 = "(You are Moderately Thin!!)"
-                        break;  
-                    case BMI > 17 && BMI < 18.5:
-                        summary1 = "You are Underweight!"
-                        summary2 = "(You are Mildly Thin!!)"
-                        break;    
-                    case BMI > 18.5 && BMI < 25:
-                        summary1 = "You are Normal."
-                        break;    
-                    case BMI > 25 && BMI < 30:
-                        summary1 = "You are Overweight!"
-                        break;    
-                    case BMI > 30 && BMI < 35:
-                        summary1 = "You are Obese!"
-                        summary2 = "(Class I Obesity!)"
-                        break;    
-                    case BMI > 35 && BMI < 40:
-                        summary1 = "You are Obese!"
-                        summary2 = "(Class II Obesity!)"
-                        break;    
-                    case BMI >= 40:
-                        summary1 = "You are Obese!"
-                        summary2 = "(Class III Obesity!)"
-                        break; 
-                }
-            } 
-            conclusion.innerHTML = `<span>${summary1}</span><span>${summary2}</span>`
-        
-    
-        // reset -->
-    
-        const nav = document.querySelector("nav")
-        nav.style.visibility = "visible" // once a calculation is done, it should be displayed
-    
-        reset.addEventListener("click", () => {
-            height.value = ""
-            weight.value = ""
-            Array.from(result).map((ele) => {
-               ele.innerHTML = ""
-            })
-            conclusion.textContent = ""
-    
-            nav.style.visibility = "hidden" // once everything is set to their initial value, the reset button should again disappear!
+    let BMI = (weight / (height * height)).toFixed(2)
+    for (let i = 0; i < result.length; i++) {
+        const element = result[indexNumber] // Specific index number for a specific calculator
+        if (!(weight.value === "" || height.value === "")) {
+            if (!(weight.value <= 0 || height.value <= 0)) {
+                element.innerHTML = `BMI: ${BMI} kg/m<sup>2</sup>`
+            } else {
+                element.innerHTML = "Please enter a valid number!" // If a negative integer or 0 has been input
+            }
+        } else {
+            element.innerHTML = "Please fill all the required fields!" // If either of the input fields is empty
+        }
+    }
+
+    // Conclusion to display after the result --->
+
+    let summary1 = ""
+    let summary2 = ""
+
+    if (!(weight.value === "" || height.value === "") && !(weight.value <= 0 || height.value <= 0)) {
+        switch (true) {
+            case BMI > 0 && BMI < 16:
+                summary1 = "You are Underweight!"
+                summary2 = "(You are Severely Thin!!)"
+                break      
+            case BMI > 16 && BMI < 17:
+                summary1 = "You are Underweight!"
+                summary2 = "(You are Moderately Thin!!)"
+                break  
+            case BMI > 17 && BMI < 18.5:
+                summary1 = "You are Underweight!"
+                summary2 = "(You are Mildly Thin!!)"
+                break    
+            case BMI > 18.5 && BMI < 25:
+                summary1 = "You are Normal."
+                break    
+            case BMI > 25 && BMI < 30:
+                summary1 = "You are Overweight!"
+                break    
+            case BMI > 30 && BMI < 35:
+                summary1 = "You are Obese!"
+                summary2 = "(Class I Obesity!)"
+                break    
+            case BMI > 35 && BMI < 40:
+                summary1 = "You are Obese!"
+                summary2 = "(Class II Obesity!)"
+                break    
+            case BMI >= 40:
+                summary1 = "You are Obese!"
+                summary2 = "(Class III Obesity!)"
+                break 
+        }
+    } 
+
+    conclusion.innerHTML = `<span>${summary1}</span><span>${summary2}</span>`
+
+    // Reset --->
+
+    const nav = document.querySelector("nav")
+    nav.style.visibility = "visible" // Once a calculation is done, it should be displayed
+
+    reset.addEventListener("click", () => {
+        height.value = ""
+        weight.value = ""
+
+        Array.from(result).map((ele) => {
+            ele.innerHTML = ""
         })
+
+        conclusion.textContent = ""
+
+        nav.style.visibility = "hidden" // Once everything is set to their initial value, the reset button should again disappear!
+    })
 }    
 
-// different calculators --->
+// Different calculators --->
 
 general.addEventListener("click", () => {
     generalCalculation = true
     metricCalculation = false
+
     let general = document.getElementById("general-calculator")
     general.style.visibility = "visible"
+
     let metric = document.querySelector("#metric-calculator")
     metric.style.visibility = "hidden"
 })
@@ -121,9 +172,10 @@ general.addEventListener("click", () => {
 metric.addEventListener("click", () => {
     metricCalculation = true
     generalCalculation = false
+
     let general = document.getElementById("general-calculator")
     general.style.visibility = "hidden"
+
     let metric = document.querySelector("#metric-calculator")
     metric.style.visibility = "visible"
 })
-
