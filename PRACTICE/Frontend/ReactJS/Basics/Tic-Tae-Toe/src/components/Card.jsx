98c35b8c185176da237227da301ba6205b352e7a
Ritik Mahapatra <ritikprofessional111@gmail.com>
2024-06-20 02:07:34 +0530

    Completed the UI part of my tic tae toe

---
+++ PRACTICE/Frontend/ReactJS/Basics/Tic-Tae-Toe/src/components/Card.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { SlGameController } from "react-icons/sl";
+
+
+function Card() {
+  return (
+    <div id='card'>
+      <SlGameController />
+    </div>
+  )
+}
+
+export default Card
