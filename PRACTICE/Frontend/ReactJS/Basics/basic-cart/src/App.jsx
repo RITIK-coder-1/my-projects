129fe8ce4a203fd87e9ae66ea9461917bcfdf67a
Ritik Mahapatra <ritikprofessional111@gmail.com>
2024-06-19 15:35:50 +0530

    finished the basic cart project

--- PRACTICE/Frontend/ReactJS/Basics/basic-cart/src/App.jsx
+++ PRACTICE/Frontend/ReactJS/Basics/basic-cart/src/App.jsx
@@ -2,9 +2,30 @@ import React from 'react'
 import { useState } from 'react'
 
 function App() {
+  let [currentItemNumber, increaseNumber] = useState(2) // this keeps track of the item number
+  const [arr, addEle] = useState(["Item 1", "Item 2"]) // the array of items
+
+
+  // this function adds a new item with incremented item number whenever called -->
+
+  function addItem() {
+    increaseNumber(prevNumber => {
+      const newNumber = ++prevNumber // it increases the previous number
+      addEle([...arr, `Item ${newNumber}`]) // it pushes a new value to the array
+      return newNumber // returns the new item number
+    })
+  }
+
   return (
     <>
-    <h1>hey</h1>
+      <ul>
+        {
+          arr.map(ele => {
+            return <li>{ele}</li> // returning each element within a list item
+          })
+        }
+      </ul>
+      <button type="button" onClick={addItem}>Add New Item</button>
     </>
   )
 }
