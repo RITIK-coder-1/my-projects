6f21effc0dbd4be2c956d276cc9ee2417ab4aa8f
Ritik Mahapatra <ritikprofessional111@gmail.com>
2024-06-20 21:43:56 +0530

    Implemented a lot of functionalities, it is almost completed

--- PRACTICE/Frontend/ReactJS/Basics/Tic-Tae-Toe/src/App.jsx
+++ PRACTICE/Frontend/ReactJS/Basics/Tic-Tae-Toe/src/App.jsx
@@ -1,21 +1,47 @@
-import React from 'react'
-import Card from './components/Card'
+import React, { useState } from 'react'
+import Card from './components/Card' // imported the Card component
+import { FaRegCircle } from "react-icons/fa6"; // the circle icon from react icons
+import { RxCross2 } from "react-icons/rx"; // the cross icon from react icons
 
 function App() {
+
+  let circle = "O"
+  let cross = "X"
+
+  let [turn, changeTurn] = useState(true)
+
+  // function to change the state of the turn -->
+
+  function a(){
+    console.log("clicked")
+    changeTurn(!turn)
+  }
+
+  function b(){
+    if (turn){
+      return FaRegCircle
+    } else {
+      return RxCross2
+    }
+  }
+
   return (
-    <>
-    <h1>Tic Tae Toe</h1>
+    <> 
+    
+    <h1>Tic Tae Toe</h1>  {/* heading */}
+
+    {turn ? <span>Turn: {circle}</span> : <span>Turn: {cross}</span>} {/* If turn is true, it is "O". If it is false, it is "X" */}
 
     <div id='container'>
-    <Card />
-    <Card />
-    <Card />
-    <Card />
-    <Card />
-    <Card />
-    <Card />
-    <Card />
-    <Card />
+    <Card callback = {a} Icon={b()}/>
+    <Card callback = {a} Icon={b()}/>
+    <Card callback = {a}/>
+    <Card callback = {a}/>
+    <Card callback = {a}/>
+    <Card callback = {a}/>
+    <Card callback = {a}/>
+    <Card callback = {a}/>
+    <Card callback = {a}/>
     </div>    
     </>
   )
--- PRACTICE/Frontend/ReactJS/Basics/Tic-Tae-Toe/src/components/Card.jsx
+++ PRACTICE/Frontend/ReactJS/Basics/Tic-Tae-Toe/src/components/Card.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
-import { SlGameController } from "react-icons/sl";
+import { SlGameController } from "react-icons/sl"; // imported a game sticker from react-icons
 
+function Card({callback, Icon}) {
 
-function Card() {
   return (
-    <div id='card'>
-      <SlGameController />
+    // I passed the callback argument to onClick, this argument will be the event function in the main app file
+
+    <div id='card' onClick={callback}> 
+
+      <SlGameController />  {/* used teh sticker for a better showcase of the cards*/}
+
+      <div id="card-container"> {/* container to contain the icons so that they don't get affected by the presence of the game sticker */}
+
+      {Icon && <Icon style={{height: "50px", width: "50px"}}/>} {/* Icon will be rendered only when it is not a falsy value */}
+
+      </div>
+
     </div>
   )
 }
