318d8ce8250463477417b2444a973d4982d81a0b
RITIK <ritikprofessional111@gmail.com>
2024-06-23 01:25:50 +0530

    added a review section of infinite moving cards

--- PRACTICE/Frontend/NextJS/music-landpage/src/components/ui/infinite-moving-cards.tsx
+++ PRACTICE/Frontend/NextJS/music-landpage/src/components/ui/infinite-moving-cards.tsx
@@ -7,7 +7,7 @@ import React, { useEffect, useState } from "react";
 export const InfiniteMovingCards = ({
   items,
   direction = "left",
-  speed = "fast",
+  speed = "normal",
   pauseOnHover = true,
   className,
 }: {
