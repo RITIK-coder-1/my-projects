9c7ce4fa41f29250597ecd043454d5b76b735ce6
Ritik <ritikprofessional111@gmail.comgit config --global user.email ritikprofessional111@gmail.com>
2024-06-22 18:23:26 +0530

    my hero section is done

--- PRACTICE/Frontend/NextJS/music-landpage/src/app/page.tsx
+++ PRACTICE/Frontend/NextJS/music-landpage/src/app/page.tsx
@@ -3,8 +3,8 @@ import HeroSection from "@/components/HeroSection";
 export default function Home() {
   return(
     <>
-    <main className="border border-solid border-red-900 h-full felx flex-col justify-center items-center">
-        <section className="border border-solid border-red-900  h-auto md:h-[40rem] flex flex-col justify-center items-center gap-5">
+    <main className="h-full felx flex-col justify-center items-center">
+        <section className="h-full md:h-[40rem] flex flex-col justify-center items-center gap-5">
         <HeroSection />
         </section>
     </main>
--- PRACTICE/Frontend/NextJS/music-landpage/tailwind.config.ts
+++ PRACTICE/Frontend/NextJS/music-landpage/tailwind.config.ts
@@ -1,13 +1,45 @@
 import type { Config } from "tailwindcss";
 
+const defaultTheme = require("tailwindcss/defaultTheme")
+const colors = require("tailwindcss/colors")
+const {
+  default: flattenColorPalette,
+} = require("tailwindcss/lib/util/flattenColorPalette")
+
+
 const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/utils/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
+      animation: {
+        shimmer: "shimmer 2s linear infinite",
+        spotlight: "spotlight 2s ease .75s 1 forwards",
+      },
+      keyframes: {
+        shimmer: {
+          from: {
+            backgroundPosition: "0 0",
+          },
+          to: {
+            backgroundPosition: "-200% 0",
+          },
+        },
+        spotlight: {
+          "0%": {
+            opacity: "0",
+            transform: "translate(-72%, -62%) scale(0.5)",
+          },
+          "100%": {
+            opacity: "1",
+            transform: "translate(-50%,-40%) scale(1)",
+          }
+        },        
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
@@ -17,4 +49,5 @@ const config: Config = {
   },
   plugins: [],
 };
+
 export default config;
