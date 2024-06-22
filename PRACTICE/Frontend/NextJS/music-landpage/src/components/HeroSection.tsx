ad4ab945c096f505d3d8be848cb0c8aba798b970
Ritik <ritikprofessional111@gmail.com>
2024-06-22 18:47:56 +0530

    modified

--- PRACTICE/Frontend/NextJS/music-landpage/src/components/HeroSection.tsx
+++ PRACTICE/Frontend/NextJS/music-landpage/src/components/HeroSection.tsx
@@ -10,7 +10,7 @@ function HeroSection() {
         <div className="flex flex-col justify-center items-center gap-2">
         
         <h1 className='text-white font-black text-5xl font-sans'>Welcome to Music Academy</h1>
-        <p>Master The Art of Music with our comprehensive courses!</p>
+        <p>Master The Art of Music with our comprehensive courses! a</p>
         </div>
         <button className="inline-flex h-12 animate-shimmer items-center justify-center border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-6 font-bold text-slate-400 transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50 900 rounded-3xl p-3 cursor-pointer">
           Explore
