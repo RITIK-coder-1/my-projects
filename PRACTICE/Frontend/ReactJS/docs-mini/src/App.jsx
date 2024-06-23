7b56cacc0b73d625528340eebd360343d4b75b84
RITIK <ritikprofessional111@gmail.com>
2024-06-23 13:55:40 +0530

    modified

--- PRACTICE/Frontend/ReactJS/docs-mini/src/App.jsx
+++ PRACTICE/Frontend/ReactJS/docs-mini/src/App.jsx
@@ -1,17 +1,18 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import Background from './components/Background'
 import './App.css'
 import Cards from './components/Cards'
 function App() {
+  const ref = useRef(null)
 
   return (
     <>
     <div className="relative w-full h-full">
     <Background />
-    <div className="fixed h-full w-full bg-blue-900/40 z-10 p-5 flex gap-3">
-    <Cards />
-    <Cards />
-    <Cards />
+    <div ref={ref} className="fixed h-full w-full bg-blue-900/40 z-10 p-5 flex gap-3">
+    <Cards name={"Card 1"} referencec={ref}/>
+    <Cards name={"Card 2"} referencec={ref}/>
+    <Cards name={"Card 3"} referencec={ref}/>
     </div>
     </div>
     </>
--- PRACTICE/Frontend/ReactJS/docs-mini/src/components/Cards.jsx
+++ PRACTICE/Frontend/ReactJS/docs-mini/src/components/Cards.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import {motion} from "framer-motion"
 
-function Cards({name}) {
+function Cards({name, referencec}) {
   return (
-    <motion.div drag className='w-52 h-52 bg-red-900 rounded-xl border border-solid border-teal-950 flex justify-center items-center font-sans font-bold'>
+    <motion.div drag dragConstraints={referencec} whileDrag={{scale: 1.1}} className='w-52 h-52 bg-red-900 rounded-xl border border-solid border-teal-950 flex justify-center items-center font-sans font-bold'>
         {name}
     </motion.div>
   )
