5775c5b05a7f95f54a0a5ce99b85a248f2f89f59
RITIK <ritikprofessional111@gmail.com>
2024-08-17 20:54:50 +0530

    learned the reading method

---
+++ PRACTICE/Backend/NodeJS/Basics/fs_module/fs.js
@@ -0,0 +1,19 @@
+const fs = require("fs") // this stores the file system module
+
+// READING A FILE
+
+// async way
+
+fs.readFile("fs.txt", (err, data) => {
+    if (err) {
+        console.log("There was an error in reading the file"); // if it catches an error
+    } else {
+        console.log(data.toString()); // the data has been converted to string
+    }    
+})
+
+// sync way
+
+const read = fs.readFileSync("fs.txt") // this returns the data directly
+console.log(read.toString()); // the data has been converted to string
+
