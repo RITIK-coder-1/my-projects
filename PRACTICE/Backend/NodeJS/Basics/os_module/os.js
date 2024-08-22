a81ebb506bd13335ab6fac1e4edfd8ec55509983
RITIK <ritikprofessional111@gmail.com>
2024-08-22 12:30:21 +0530

    learned the OS module

---
+++ PRACTICE/Backend/NodeJS/Basics/os_module/os.js
@@ -0,0 +1,16 @@
+const os = require("os")
+
+console.log(os.arch()); // x64 // type of architecture
+console.log(os.tmpdir()); // /tmp // temporary directory
+console.log(os.freemem()); // 1952382976 // free memory available in bytes
+console.log(os.hostname()); // penguin // name of the host
+console.log(os.totalmem()); // 2898321408 // total memory in bytes
+console.log(os.type()); // Linux // type of OS
+console.log(os.platform()); // linux
+
+
+
+
+
+
+
