841ec7324acfda2a30e15fe4a8c0db022a287e41
RITIK <ritikprofessional111@gmail.com>
2024-08-23 12:21:28 +0530

    learned the http module

--- PRACTICE/Backend/NodeJS/Basics/http_module/http.js
+++ PRACTICE/Backend/NodeJS/Basics/http_module/http.js
@@ -2,7 +2,7 @@ const http = require("http") // this stores the http module
 
 const server = http.createServer((req, res) => {
     if (req.url === "/") {
-        res.write("<h1>Hello, You created your server!") // if the request url is this, send a response of this html text
+        res.write("<h1>Hello, I created my server using NodeJS!") // if the request url is this, send a response of this html text
     }
     res.end() // once the response has been sent, end it
 })
