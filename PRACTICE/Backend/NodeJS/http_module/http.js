daf316794f7ea70e446da5bbd2bee74e26214e8d
RITIK <ritikprofessional111@gmail.com>
2024-08-26 12:44:49 +0530

    learned additional things in node server like status code and headers

--- PRACTICE/Backend/NodeJS/http_module/http.js
+++ PRACTICE/Backend/NodeJS/http_module/http.js
@@ -1,9 +1,11 @@
 const http = require("http") // this stores the http module
 
-const hostname = "127.0.0.1"
+const hostname = "localhost" // the current host
 const port = 3000 // port to listen at
 
 const server = http.createServer((req, res) => {
+    res.statusCode = 200 // the status code
+    res.setHeader("Content-Type", "text/html") // the current content that is being sent is html
     if (req.url === "/") {
         res.write("<h1>Hello, I created my server using NodeJS!") // if the request url is this, send a response of this html text
     }
