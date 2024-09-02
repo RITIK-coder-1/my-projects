e0ee441daa9ccc128d932b69f9a04bbc74bf1456
RITIK <ritikprofessional111@gmail.com>
2024-09-02 19:52:51 +0530

    express blueprint

---
+++ PRACTICE/Backend/ExpressJS/basic-server/index.js
@@ -0,0 +1,13 @@
+const express = require('express'); // Imported the Express module
+
+const app = express(); // This creates an instance of an Express application
+
+const port = 3000; // specific port
+
+app.get('/', (req, res) => { // this method works with the get requests
+  res.send('Hello, World!'); // Send a response of "Hello, World!" to the client
+});
+
+app.listen(port, () => { // this method is used to listen for requests
+  console.log(`Server is running on http://localhost:${port}`); // This logs a message indicating the server is running
+});
