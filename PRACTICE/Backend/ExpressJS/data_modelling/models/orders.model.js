969d2fe3c98d1e2a2f5b3536f002bbc10584e97d
Ritik Mahapatra <ritikprofessional111@gmail.com>
2025-02-27 16:14:43 +0530

    created an order schema

---
+++ PRACTICE/Backend/ExpressJS/data_modelling/models/orders.model.js
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+
+const orderSchema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true 
+    },
+    products: [{
+        productId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Product",
+            required: true
+        },
+        quantity: {
+            type: Number,
+            required: true,
+            min: 1
+        }
+    }],
+    totalPrice: {
+        type: Number,
+        min: 1,
+        required: true
+    }, 
+    status: {
+        type: String,
+        enum: ["pending", "shipped", "delivered", "cancelled"],
+        default: "pending"
+    }
+}, {
+    timestamps: true
+});
+
+const Order = mongoose.model("Order", orderSchema);
+
+export default Order;
