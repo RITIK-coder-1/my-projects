931fb85822ad31cd3c60b07ed5aa3826d2509896
Ritik Mahapatra <ritikprofessional111@gmail.com>
2025-02-27 14:58:37 +0530

    created a todo schema

--- PRACTICE/Backend/ExpressJS/data_modelling/models/todos.model.js
+++ PRACTICE/Backend/ExpressJS/data_modelling/models/todos.model.js
@@ -1,1 +1,23 @@
-import mongoose from "mongoose" // importing mongoose
+import mongoose from "mongoose" 
+
+const todoSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    complete: {
+        type: Boolean,
+        default: false
+    },
+    createdBy: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    }
+}, {
+    timestamps: true
+})
+
+const Todo = mongoose.model("Todo", todoSchema)
+
+export default Todo
