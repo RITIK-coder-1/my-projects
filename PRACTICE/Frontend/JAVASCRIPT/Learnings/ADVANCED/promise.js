dbc31934f25face3a782e68f914fb7cfa0586242
Ritik Mahapatra <ritikprofessional111@gmail.com>
2025-03-04 13:52:31 +0530

    revised async and await properly

--- PRACTICE/Frontend/JAVASCRIPT/Learnings/ADVANCED/promise.js
+++ PRACTICE/Frontend/JAVASCRIPT/Learnings/ADVANCED/promise.js
@@ -13,3 +13,28 @@ const myPromise = new Promise((resolve, reject) => {
 myPromise
 .then((data) => data + " returned again")
 .then((data) => console.log(data)) // promise resolved returned again
+
+// async and await
+
+async function consumePromise(){
+    try {
+        let data = await new Promise((resolve, reject) => {
+            setTimeout(() => {
+                console.log("executed")
+                resolve("promise resolved")
+                reject(Error)
+            }, 2000)
+        })
+
+        console.log(data)
+
+    } catch (error) {
+        console.log("Promise is rejected: ", error)
+    }    
+}
+
+consumePromise() // promise resolved
+
+
+
+
