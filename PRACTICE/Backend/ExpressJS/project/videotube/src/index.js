e305c65285be06928243686e4737947f42f05451
Ritik Mahapatra <ritikprofessional111@gmail.com>
2025-03-13 13:26:48 +0530

    updated the code

--- PRACTICE/Backend/ExpressJS/project/videotube/src/index.js
+++ PRACTICE/Backend/ExpressJS/project/videotube/src/index.js
@@ -4,17 +4,23 @@ import 'dotenv/config' // importing the environment variables
 import express from "express" // importing the express
 
 const app = express() // the express app
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000 // the port number
+
+// handles any unwanted error before the database connection
+
+app.on("error", (error) => {
+    console.log("There was an error in the server listening to the database :", error)                
+})
+
+// Immediately Invoked Function to connect DB
 
 ;(
     async function connectDB() {
         try {
+            // connecting to the database
             await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
 
-            app.on("error", (error) => {
-                console.log("There was an error in the server listening to the database :", error)                
-            })
-
+            // starts listening
             app.listen(port, () => {
                 console.log("Our express app has been started!")                
             })
