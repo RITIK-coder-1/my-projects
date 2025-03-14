f57e1a2d3eb79eba0144d22a7dad7365fb93855f
Ritik Mahapatra <ritikprofessional111@gmail.com>
2025-03-14 16:21:03 +0530

    installed mongoose aggregate

--- PRACTICE/Backend/ExpressJS/project/videotube/src/models/videos.model.js
+++ PRACTICE/Backend/ExpressJS/project/videotube/src/models/videos.model.js
@@ -1,4 +1,5 @@
-import mongoose from "mongoose"
+import mongoose, { Aggregate } from "mongoose"
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2"
 
 const videoSchema = new mongoose.Schema({
     watchHistory: [
