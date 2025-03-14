3a4548d691951d93f931ec109223ca6fbed2e0c9
Ritik Mahapatra <ritikprofessional111@gmail.com>
2025-03-14 15:49:29 +0530

    created a user model for videotube

---
+++ PRACTICE/Backend/ExpressJS/project/videotube/src/models/videos.model.js
@@ -0,0 +1,54 @@
+import mongoose from "mongoose"
+
+const videoSchema = mongoose.Schema({
+    watchHistory: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Video",
+            required: true
+        }
+    ],
+    username: {
+        type: String,
+        required: true,
+        lowercase: true,
+        unique: true,
+        trim: true,
+        index: true
+    },
+    fullname: {
+        type: String,
+        required: true,
+        trim: true,
+        index: true
+    },
+    email: {
+        type: String,
+        required: true,
+        lowercase: true,
+        unique: true,
+        trim: true,
+        index: true
+    }, 
+    avatar: {
+        type: String, // URL from a different service (Cloudinary)
+        required: true,
+    }, 
+    coverImage: {
+        type: String, // URL from a different service (Cloudinary)
+        required: true,
+    }, 
+    password: {
+        type: String,
+        required: true,
+    }, 
+    refreshToken: {
+        type: String,
+    }
+}, {
+    timestamps: true
+})
+
+const Video = mongoose.model("Video", videoSchema)
+
+export default Video
