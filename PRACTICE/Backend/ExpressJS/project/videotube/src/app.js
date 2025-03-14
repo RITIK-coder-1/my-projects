05fe9c80f772c4f9e5995439b355c35b97b61506
Ritik Mahapatra <ritikprofessional111@gmail.com>
2025-03-14 13:15:53 +0530

    learned cors, cookie-parser and async-handler

--- PRACTICE/Backend/ExpressJS/project/videotube/src/app.js
+++ PRACTICE/Backend/ExpressJS/project/videotube/src/app.js
@@ -1,7 +1,26 @@
-import express from "express"
-import cors from "cors"
-import cookieParser from "cookie-parser"
+import express from "express" // importing express
+import cors from "cors" // importing cors for security
+import cookieParser from "cookie-parser" // importing cookie-parser
+import "dotenv/config" // importing environment variables
 
-const app = express()
+const app = express() // the express app
+const limit = "16kb" // setting the limit of accepting data
+
+// setting the cors origin
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}))
+
+// accepting different data
+app.use(express.json({
+    limit: limit
+}))
+app.use(express.urlencoded({
+    limit: limit,
+    extended: true
+})) 
+app.use(express.static("public"))
+app.use(cookieParser())
 
 export default app
---
+++ PRACTICE/Backend/ExpressJS/project/videotube/src/utils/asyncHandler.js
@@ -0,0 +1,14 @@
+function asyncHandler(func){
+    return async (req, res, next) => {
+        try {
+            await func(req, res, next)
+        } catch (error) {
+            res
+            .status(error.code || 500)
+            .json({
+                success: false,
+                message: error.message 
+            })         
+        }
+    }
+}
