880d5d50d2ba7795898d59f8807051a162ce1876
Ritik Mahapatra <ritikprofessional111@gmail.com>
2025-03-19 20:17:26 +0530

    wrote the entire logic to upload files on cloudinary

--- PRACTICE/Backend/ExpressJS/project/videotube/src/utils/cloudinary.js
+++ PRACTICE/Backend/ExpressJS/project/videotube/src/utils/cloudinary.js
@@ -1,10 +1,33 @@
-import { v2 as cloudinary } from 'cloudinary'
+import { v2 as cloudinary } from "cloudinary"
 import fs from "fs"
 import "dotenv/config"
 
 // configuration
-cloudinary.config({ 
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
-    api_key: process.env.CLOUDINARY_API_KEY, 
-    api_secret: process.env.CLOUDINARY_API_SECRET
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 })
+
+// Upload On Cloudinary
+const uploadOnCloudinary = async (filePath) => {
+    if (!filePath) return null
+
+    try {
+        const response = await cloudinary.uploader.upload(filePath, { resource_type: "auto" })
+        console.log("Upload Successful:", response.url)
+        return response
+    } catch (error) {
+        fs.unlink(filePath, (err) => {
+            if (err) {
+                console.error("File deletion failed:", err.message)
+            } else {
+                console.log("The file has been deleted successfully!")
+            }
+        })
+        console.error("Upload Failed:", error)
+        return null
+    }
+}
+
+export default uploadOnCloudinary
