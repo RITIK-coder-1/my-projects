0b9b7df5938dcf77adf5828c772cddfb9c805d37
Ritik Mahapatra <ritikprofessional111@gmail.com>
2025-03-26 13:04:28 +0530

    injected JS inside my Hello component

--- PRACTICE/Frontend/ReactJS/basic-react-app/src/App.jsx
+++ PRACTICE/Frontend/ReactJS/basic-react-app/src/App.jsx
@@ -1,11 +1,7 @@
 import Hello from "./components/Hello";
 
 function App() {
-  return (
-    <>
-      <Hello />
-    </>
-  );
+  return Hello();
 }
 
 export default App;
--- PRACTICE/Frontend/ReactJS/basic-react-app/src/components/Hello.jsx
+++ PRACTICE/Frontend/ReactJS/basic-react-app/src/components/Hello.jsx
@@ -1,7 +1,13 @@
 function Hello() {
+  const name = "Ritik";
+  const nameArray = Array.from(name);
+  const element = nameArray.map((ele) => {
+    return <p>{ele}</p>;
+  });
   return (
     <>
-      <h1>Hey, how are you? I'm Ritik!</h1>
+      <h1>Hey, how are you? I'm {name}!</h1>
+      {element}
     </>
   );
 }
