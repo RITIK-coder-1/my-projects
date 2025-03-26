ecdbbcc541f1008ae5a9a5265cd8681d11f42990
Ritik Mahapatra <ritikprofessional111@gmail.com>
2025-03-26 13:23:06 +0530

    updated

--- PRACTICE/Frontend/ReactJS/basic-react-app/src/App.jsx
+++ PRACTICE/Frontend/ReactJS/basic-react-app/src/App.jsx
@@ -1,7 +1,12 @@
-import Hello from "./components/Hello";
-
 function App() {
-  return Hello();
+  let counter = 0;
+  return (
+    <>
+      <h1 style={{ color: "aliceblue" }}>Basic Counter: {counter}</h1>
+      <button>Increase</button>
+      <button>Decrease</button>
+    </>
+  );
 }
 
 export default App;
