7a34f7899e316b27074f2ca85f4db854ef4d1938
Ritik Mahapatra <ritikprofessional111@gmail.com>
2025-03-29 00:03:01 +0530

    completed my password generator project

--- PRACTICE/Frontend/ReactJS/password-generator/src/App.jsx
+++ PRACTICE/Frontend/ReactJS/password-generator/src/App.jsx
@@ -10,49 +10,98 @@ function App() {
   let num = "0123456789";
   let char = "_+-*/$%^&*@!~";
 
-  for (let i = 0; i <= passwordLength; i++) {
-    const newChar = Math.floor(Math.random() * str.length + 1);
-    password = password.concat(str.charAt(newChar));
+  const checkNumber = () => {
+    setNumberAllowed((numberAllowed) => !numberAllowed);
+  };
+
+  const checkChar = () => {
+    setCharAllowed((charAllowed) => !charAllowed);
+  };
+
+  const setLength = () => {
+    setPasswordLength((passwordLength) => event.target.value);
+  };
+
+  if (numberAllowed) {
+    str = str.concat(num);
   }
 
-  const check = (param, func) => {
-    return () => {
-      func(() => !param);
-    };
+  if (charAllowed) {
+    str = str.concat(char);
+  }
+
+  const generate = () => {
+    let newPassword = "";
+    setPassword((password) => {
+      return password.replace(password, "");
+    });
+    for (let i = 0; i < passwordLength; i++) {
+      const newChar = Math.floor(Math.random() * str.length);
+      newPassword += str.charAt(newChar);
+    }
+
+    setPassword((password) => {
+      return password.concat(newPassword);
+    });
   };
+
+  const copyText = () => {
+    navigator.clipboard.writeText(password);
+  };
+
   return (
     <>
       <h1 className="text-white text-5xl font-bold">Password Generator</h1>
-      <div className="h-56 w-84 bg-red-700 p-5 flex flex-col gap-5">
-        <input
-          type="text"
-          className="border border-red border-solid bg-white rounded outline-none p-2"
-          value={password}
-          placeholder="Password"
-          readOnly
-        />
-        <div className="flex justify-between items-center">
-          <label>Length: ({passwordLength})</label>
-          <input type="range" className="cursor-pointer" />
+      <div className="h-64 w-84 bg-red-700 p-5 flex flex-col gap-5">
+        <div className="flex justify-center items-center">
+          <input
+            type="text"
+            className="border border-red border-solid bg-white rounded outline-none p-2"
+            value={password}
+            placeholder="Password"
+            readOnly
+          />
+          <span
+            className="bg-purple-800 p-2 rounded font-bold cursor-pointer"
+            onClick={copyText}
+          >
+            Copy
+          </span>
         </div>
         <div className="flex justify-between items-center">
-          <label>Allow Numbers: </label>
+          <label className="text-amber-200 font-bold">
+            Length: {passwordLength}
+          </label>
+          <input
+            type="range"
+            className="cursor-pointer"
+            onChange={setLength}
+            value={passwordLength}
+          />
+        </div>
+        <div className="flex justify-between items-center">
+          <label className="text-amber-200 font-bold">Allow Numbers: </label>
           <input
             type="checkbox"
             className="cursor-pointer"
             checked={numberAllowed}
-            onClick={check(numberAllowed, setNumberAllowed)}
+            onChange={checkNumber}
           />
         </div>
         <div className="flex justify-between items-center">
-          <label>Allow Special Characters: </label>
+          <label className="text-amber-200 font-bold">
+            Allow Special Characters:{" "}
+          </label>
           <input
             type="checkbox"
             className="cursor-pointer"
             checked={charAllowed}
-            onClick={check(charAllowed, setCharAllowed)}
+            onChange={checkChar}
           />
         </div>
+        <button onClick={generate} className="bg-purple-600 ">
+          Generate Password
+        </button>
       </div>
     </>
   );
