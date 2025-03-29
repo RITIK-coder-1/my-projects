82ab684c5c9ef499f5beb90d7dffc0444f2d6db4
Ritik Mahapatra <ritikprofessional111@gmail.com>
2025-03-29 22:33:56 +0530

    added commnets on my password generator

--- PRACTICE/Frontend/ReactJS/password-generator/src/App.jsx
+++ PRACTICE/Frontend/ReactJS/password-generator/src/App.jsx
@@ -1,6 +1,8 @@
 import { useState, useCallback, useEffect, useRef } from "react";
 
 function App() {
+  // states to keep track of password and its dependencies
+
   let [password, setPassword] = useState("");
   let [passwordLength, setPasswordLength] = useState(8);
   let [numberAllowed, setNumberAllowed] = useState(true);
@@ -8,44 +10,53 @@ function App() {
 
   const passwordRef = useRef(null);
 
+  // it checks and unchecks the number option with useCallback optimization
   const checkNumber = useCallback(() => {
     setNumberAllowed((numberAllowed) => !numberAllowed);
   }, [numberAllowed]);
 
+  // it checks and unchecks the char option with useCallback optimization
   const checkChar = useCallback(() => {
     setCharAllowed((charAllowed) => !charAllowed);
   }, [charAllowed]);
 
+  // it sets the length of password with useCallback optimization
   const setLength = useCallback((event) => {
     setPasswordLength(event.target.value);
   }, []);
 
+  // this is the function that generates a random password
   const generate = useCallback(() => {
-    let str = "abcdefghijklmnopqrstwvuxyzABCDEFGHIJKLMNOPQURSTUVWXYZ";
+    let str = "abcdefghijklmnopqrstwvuxyzABCDEFGHIJKLMNOPQURSTUVWXYZ"; // the string that creates a random password
     let num = "0123456789";
     let char = "_+-*/$%^&*@!~";
-    let newPassword = "";
+    let newPassword = ""; // temporary password
 
+    // if number is allowed, include all the numbers in the string
     if (numberAllowed) {
       str = str.concat(num);
     }
 
+    // if characters are allowed, include all the characters in the string
     if (charAllowed) {
       str = str.concat(char);
     }
 
     for (let i = 0; i < passwordLength; i++) {
-      const newChar = Math.floor(Math.random() * str.length);
-      newPassword += str.charAt(newChar);
+      const newChar = Math.floor(Math.random() * str.length); // random number
+      newPassword += str.charAt(newChar); // new password
     }
 
+    // the new password is set
     setPassword(newPassword);
-  }, [numberAllowed, charAllowed, passwordLength]);
+  }, [numberAllowed, charAllowed, passwordLength]); // the dependencies
 
+  // I want to render the generate function as soon as the component mounts, thus I used useEffect
   useEffect(() => {
     generate();
   }, [numberAllowed, charAllowed, passwordLength]);
 
+  // function to copy the password to clipboard
   const copyText = useCallback(() => {
     passwordRef.current?.select();
     navigator.clipboard.writeText(password);
