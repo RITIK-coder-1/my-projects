cf06f58b64f83030de20ea32a0b9158da15f9dbb
Ritik Mahapatra <ritikprofessional111@gmail.com>
2025-06-11 12:33:12 +0530

    revised basic react concepts

--- PRACTICE/Frontend/ReactJS/basic-counter/src/components/Button.jsx
+++ PRACTICE/Frontend/ReactJS/basic-counter/src/components/Button.jsx
@@ -1,10 +1,16 @@
+let props = {
+  name,
+};
+
 function Button({ content = "Click Me", onClick }) {
+  let n = props.name;
   return (
     <button
       className="relative z-0 rounded bg-pink-500 px-10 py-3 transition-[all_0.3s_ease] after:absolute after:left-0 after:top-0 after:-z-10 after:h-full after:w-0 after:rounded after:bg-pink-700 after:transition-[all_0.3s_ease]  hover:after:w-full m-2 cursor-pointer"
       onClick={onClick}
     >
       {content}
+      {n}
     </button>
   );
 }
