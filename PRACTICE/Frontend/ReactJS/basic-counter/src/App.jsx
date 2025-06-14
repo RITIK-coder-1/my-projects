576e1d9cda69f785eac4e25d7a13f9ceceb888c8
Ritik Mahapatra <ritikprofessional111@gmail.com>
2025-06-14 12:37:05 +0530

    updated

--- PRACTICE/Frontend/ReactJS/basic-counter/src/App.jsx
+++ PRACTICE/Frontend/ReactJS/basic-counter/src/App.jsx
@@ -1,11 +1,9 @@
 import Counter from "./components/Counter";
-import Demo from "./components/Demo";
 
 function App() {
   return (
     <>
       <Counter />
-      {/* <Demo /> */}
     </>
   );
 }
