6c806df4deb3f1dbb384d8153c804cc0357a4d94
Ritik Mahapatra <ritikprofessional111@gmail.com>
2025-06-23 12:04:28 +0530

    made my input box dynamic. Included a lot of props

--- PRACTICE/Frontend/ReactJS/currency-converter/src/components/Input.jsx
+++ PRACTICE/Frontend/ReactJS/currency-converter/src/components/Input.jsx
@@ -1,6 +1,5 @@
-import useCurrency from "../hooks/useCurrency";
-
 function Input({
+  label,
   dataArray = [],
   amount,
   onAmountChange,
@@ -12,16 +11,26 @@ function Input({
   return (
     <div className="w-3xl h-36 bg-white p-3 rounded-xl flex justify-between">
       <div className="flex flex-col justify-center h-full gap-6">
-        <label className="text-gray-500 font-bold text-xl">From</label>
+        <label className="text-gray-500 font-bold text-xl">{label}</label>
         <input
           type="text"
           className="w-10 outline-none font-bold text-xl"
           defaultValue="0"
+          disabled={amountDisable}
+          value={amount}
+          onChange={(e) =>
+            onAmountChange && onAmountChange(Number(e.target.value))
+          }
         />
       </div>
       <div className="flex flex-col justify-center h-full gap-6 items-end">
         <label className="text-gray-500 font-bold text-xl">Currency Type</label>
-        <select className="w-30 outline-none font-thin text-md font-bold p-3 rounded-md bg-gray-100">
+        <select
+          className="w-30 outline-none font-thin text-md font-bold p-3 rounded-md bg-gray-100"
+          value={selectCurrency}
+          onChange={(e) => onCurrencyChange && onCurrencyChange(e.target.value)}
+          disabled={currencyDisable}
+        >
           {dataArray.map((ele) => {
             return (
               <option key={ele} value={ele}>
