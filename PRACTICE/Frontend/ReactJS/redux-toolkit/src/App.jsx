f319de4934e2f7e84cdf976d78b52813382f8ff4
Ritik Mahapatra <ritikprofessional111@gmail.com>
2025-10-15 18:40:34 +0530

    completed my counter app using redux

--- PRACTICE/Frontend/ReactJS/redux-toolkit/src/App.jsx
+++ PRACTICE/Frontend/ReactJS/redux-toolkit/src/App.jsx
@@ -1,10 +1,47 @@
 import "./App.css";
+import React, { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import {
+  increment,
+  decrement,
+  incrementByAmount,
+} from "./redux/features/counterSlice";
 
 function App() {
+  const count = useSelector((state) => state.counter.value);
+  const dispatch = useDispatch();
+  const [amount, setAmount] = useState(0);
   return (
-    <>
-      <h1 className="text-2xl">hello</h1>
-    </>
+    <div className="text-center mt-5">
+      <h1>Counter: {count}</h1>
+      <button
+        onClick={() => dispatch(increment())}
+        className="border border-black border-solid mr-5 p-1"
+      >
+        Increment
+      </button>
+      <button
+        onClick={() => dispatch(decrement())}
+        className="border border-black border-solid p-1"
+      >
+        Decrement
+      </button>
+      <div className="mt-5">
+        <input
+          type="number"
+          value={amount}
+          onChange={(e) => setAmount(Number(e.target.value))}
+          placeholder="Enter amount"
+          className="border border-black border-solid p-1"
+        />
+        <button
+          onClick={() => dispatch(incrementByAmount(amount))}
+          className="border border-black border-solid p-1"
+        >
+          Add Amount
+        </button>
+      </div>
+    </div>
   );
 }
 
---
+++ PRACTICE/Frontend/ReactJS/redux-toolkit/src/redux/features/counterSlice.js
@@ -0,0 +1,25 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const counterSlice = createSlice({
+  name: "counter",
+  initialState: {
+    value: 0,
+  },
+  reducers: {
+    increment: (state) => {
+      state.value += 1;
+    },
+    decrement: (state) => {
+      state.value -= 1;
+    },
+    incrementByAmount: (state, action) => {
+      state.value += action.payload;
+    },
+  },
+});
+
+// export actions (auto generated)
+export const { increment, decrement, incrementByAmount } = counterSlice.actions;
+
+// expore the reducer to configure store
+export default counterSlice.reducer;
