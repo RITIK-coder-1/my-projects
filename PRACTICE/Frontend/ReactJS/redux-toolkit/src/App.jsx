827e8caf47a81cc7aca7159dc4bae1108889de46
Ritik Mahapatra <ritikprofessional111@gmail.com>
2025-10-15 21:30:15 +0530

    added more buttons to learn redux toolkit better

--- PRACTICE/Frontend/ReactJS/redux-toolkit/src/App.jsx
+++ PRACTICE/Frontend/ReactJS/redux-toolkit/src/App.jsx
@@ -5,40 +5,78 @@ import {
   increment,
   decrement,
   incrementByAmount,
+  decrementByAmount,
+  reset,
 } from "./redux/features/counterSlice";
 
 function App() {
   const count = useSelector((state) => state.counter.value);
   const dispatch = useDispatch();
-  const [amount, setAmount] = useState(0);
+  const [amount1, setAmount1] = useState("");
+  const [amount2, setAmount2] = useState("");
   return (
     <div className="text-center mt-5">
-      <h1>Counter: {count}</h1>
+      <h1 className="text-white">Counter: {count}</h1>
       <button
         onClick={() => dispatch(increment())}
-        className="border border-black border-solid mr-5 p-1"
+        className="border border-white border-solid mr-5 p-1 text-white"
       >
         Increment
       </button>
       <button
         onClick={() => dispatch(decrement())}
-        className="border border-black border-solid p-1"
+        className="border border-white border-solid p-1 text-white"
       >
         Decrement
       </button>
+      <button
+        onClick={() => {
+          dispatch(reset());
+          setAmount1((amount1) => (amount1 = ""));
+          setAmount2((amount2) => (amount2 = ""));
+        }}
+        className="border border-white border-solid p-1 ml-5 text-white"
+      >
+        Reset
+      </button>
       <div className="mt-5">
         <input
-          type="number"
-          value={amount}
-          onChange={(e) => setAmount(Number(e.target.value))}
+          type="text"
+          value={amount1}
+          onChange={(e) => {
+            setAmount1((amount1) => (amount1 = Number(e.target.value)));
+            if (e.target.value < 0) {
+              setAmount1((amount1) => (amount1 = "0"));
+            }
+          }}
           placeholder="Enter amount"
-          className="border border-black border-solid p-1"
+          className="border border-white border-solid p-1 text-white"
         />
         <button
-          onClick={() => dispatch(incrementByAmount(amount))}
-          className="border border-black border-solid p-1"
+          onClick={() => dispatch(incrementByAmount(amount1))}
+          className="border border-white border-solid p-1 text-white"
         >
           Add Amount
+        </button>
+      </div>
+      <div className="mt-5">
+        <input
+          type="text"
+          value={amount2}
+          onChange={(e) => {
+            setAmount2((amount2) => (amount2 = Number(e.target.value)));
+            if (e.target.value < 0) {
+              setAmount2((amount2) => (amount2 = "0"));
+            }
+          }}
+          placeholder="Enter amount"
+          className="border border-white border-solid p-1 text-white"
+        />
+        <button
+          onClick={() => dispatch(decrementByAmount(amount2))}
+          className="border border-white border-solid p-1 text-white"
+        >
+          Deduct Amount
         </button>
       </div>
     </div>
--- PRACTICE/Frontend/ReactJS/redux-toolkit/src/redux/features/counterSlice.js
+++ PRACTICE/Frontend/ReactJS/redux-toolkit/src/redux/features/counterSlice.js
@@ -15,11 +15,23 @@ const counterSlice = createSlice({
     incrementByAmount: (state, action) => {
       state.value += action.payload;
     },
+    decrementByAmount: (state, action) => {
+      state.value -= action.payload;
+    },
+    reset: (state) => {
+      state.value = 0;
+    },
   },
 });
 
 // export actions (auto generated)
-export const { increment, decrement, incrementByAmount } = counterSlice.actions;
+export const {
+  increment,
+  decrement,
+  incrementByAmount,
+  decrementByAmount,
+  reset,
+} = counterSlice.actions;
 
 // expore the reducer to configure store
 export default counterSlice.reducer;
