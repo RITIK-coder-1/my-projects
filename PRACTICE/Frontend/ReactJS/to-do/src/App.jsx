667c01069aeb10bfd01b56ddf2e958be51eff86f
Ritik Mahapatra <ritikprofessional111@gmail.com>
2025-10-16 22:57:39 +0530

    started a new todo project to polish redux

---
+++ PRACTICE/Frontend/ReactJS/to-do/src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-center text-black text-3xl text-white">TO DO</h1>
+      <form action="#">
+        <input
+          type="text"
+          placeholder="Type Your To-Do"
+          className="border-solid border-white border-2"
+        />
+      </form>
+    </>
+  );
+}
+
+export default App;
